ef08a030fd2fddd2ec79f947168b3a432d99a022
Yoo TaeSeung <remicon99@gmail.com>
2025-02-03 18:43:27 +0900

    setting: svgr 세팅

---
+++ apps/admin/src/assets/svg/index.ts
@@ -0,0 +1,1 @@
+export { default as IcSignOut } from './IcSignOut'
--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  plugins: [TanStackRouterVite({}), react()],
+  plugins: [TanStackRouterVite({}), react(), svgr()],
 });
