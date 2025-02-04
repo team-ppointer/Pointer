cb475bbd9aab31523ece43cc841eadabc494fe22
Yoo TaeSeung <remicon99@gmail.com>
2025-02-04 17:47:15 +0900

    setting: path alias 세팅

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  plugins: [TanStackRouterVite({}), react(), svgr()],
+  plugins: [TanStackRouterVite({}), react(), svgr(), tsconfigPaths()],
 });
