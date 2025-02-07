30e5dc326ae8879f42a06584437be1ff4a5db08e
Yoo TaeSeung <remicon99@gmail.com>
2025-02-07 17:41:03 +0900

    refactor: 컴포넌트 import/export 폴더별로 묶는 방식으로 리팩토링

--- apps/admin/src/components/index.ts
+++ apps/admin/src/components/index.ts
@@ -1,19 +1,17 @@
-import Button from './Buttons/Button';
-import IconButton from './Buttons/IconButton';
-import PrevPageButton from './Buttons/PrevPageButton';
-import PlusButton from './Buttons/PlusButton';
-import FloatingButton from './Buttons/FloatingButton';
-import DeleteButton from './Buttons/DeleteButton';
+import {
+  Button,
+  DeleteButton,
+  FloatingButton,
+  IconButton,
+  PlusButton,
+  PrevPageButton,
+} from './Buttons';
+import { AnswerInput, Input, LevelSelect, SearchInput, TagSelect } from './Inputs';
 import GNBMenu from './GNBMenu';
 import StatusToggle from './StatusToggle';
 import Modal from './Modal';
 import Tag from './Tag';
 import ProblemCard from './ProblemCard';
-import Input from './Inputs/Input';
-import SearchInput from './Inputs/SearchInput';
-import TagSelect from './Inputs/TagSelect';
-import AnswerInput from './Inputs/AnswerInput';
-import LevelSelect from './Inputs/LevelSelect';
 import Calendar from './Calandar';
 
 export {
