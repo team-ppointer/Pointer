459f51cb0b1fc6a16aae1b7ad53adc885ea30fb2
Yoo TaeSeung <remicon99@gmail.com>
2025-02-07 18:50:55 +0900

    refactor: IconButton 리팩토링 (통으로 svg 추출, selectIcon 색상 변경)

--- apps/admin/src/components/Buttons/IconButton.tsx
+++ apps/admin/src/components/Buttons/IconButton.tsx
@@ -1,51 +1,33 @@
-import React from 'react';
 import { ButtonHTMLAttributes } from 'react';
-// Import icon components
 import {
-  IcLeft,
-  IcRight,
-  IcView,
-  IcPreview,
-  IcModify,
-  IcDelete,
-  IcSelect,
-  IcUnselected,
+  IcLeftButton,
+  IcRightButton,
+  IcViewButton,
+  IcPreviewButton,
+  IcModifyButton,
+  IcDeleteButton,
+  IcSelectButton,
+  IcUnselectedButton,
 } from '@svg';
 
 interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant: 'left' | 'right' | 'view' | 'preview' | 'modify' | 'delete' | 'select' | 'unselected';
 }
 
-const IconButton: React.FC<IconButtonProps> = ({ variant, className, onClick, ...props }) => {
-  const baseStyles = 'w-[3.6rem] h-[3.6rem] rounded-[4px] flex items-center justify-center';
-
-  const variantStyles = {
-    left: 'bg-darkgray100',
-    right: 'bg-darkgray100',
-    view: 'bg-lightgreen',
-    preview: 'bg-lightgray300',
-    modify: 'bg-lightblue',
-    delete: 'bg-lightred',
-    select: 'bg-blue',
-    unselected: 'bg-lightgray300',
-  };
-
+const IconButton = ({ variant, className, onClick, ...props }: IconButtonProps) => {
   const icons = {
-    left: <IcLeft width={11} height={21} />,
-    right: <IcRight width={11} height={21} />,
-    view: <IcView width={24} height={24} />,
-    preview: <IcPreview width={24} height={24} />,
-    modify: <IcModify width={24} height={24} />,
-    delete: <IcDelete width={24} height={24} />,
-    select: <IcSelect width={24} height={24} />,
-    unselected: <IcUnselected width={24} height={24} />,
+    left: <IcLeftButton width={36} height={36} />,
+    right: <IcRightButton width={36} height={36} />,
+    view: <IcViewButton width={36} height={36} />,
+    preview: <IcPreviewButton width={36} height={36} />,
+    modify: <IcModifyButton width={36} height={36} />,
+    delete: <IcDeleteButton width={36} height={36} />,
+    select: <IcSelectButton width={36} height={36} />,
+    unselected: <IcUnselectedButton width={36} height={36} />,
   };
 
   return (
-    <button
-      className={`${baseStyles} ${variantStyles[variant]} ${className}`}
-      onClick={onClick}
-      {...props}>
+    <button className={`${className}`} onClick={onClick} {...props}>
       {icons[variant]}
     </button>
   );
