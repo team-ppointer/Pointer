0ba2e5abf71aca8afa6a0a785adc1ee1897e6c5f
Yoo TaeSeung <remicon99@gmail.com>
2025-02-07 15:56:15 +0900

    feat: 캘린더 - 이전날짜 발행 버튼 삭제

--- apps/admin/src/components/Calandar.tsx
+++ apps/admin/src/components/Calandar.tsx
@@ -9,12 +9,16 @@ dayjs.locale('ko');
 
 interface DayProps extends HTMLAttributes<HTMLDivElement> {
   variant?: 'thisMonth' | 'anotherMonth';
+  fullDate: string;
   day: number;
   dayOfWeek?: number; // 요일 - 0: Sunday ~ 6: Saturday
   event?: string;
 }
 
-const Day = ({ variant = 'thisMonth', day, dayOfWeek, event }: DayProps) => {
+const Day = ({ variant = 'thisMonth', fullDate, day, dayOfWeek, event }: DayProps) => {
+  const today = dayjs().startOf('day');
+  const isPast = dayjs(fullDate).isBefore(today, 'day');
+
   const dayOfWeekStyle = dayOfWeek === 0 ? 'text-red' : dayOfWeek === 6 ? 'text-blue' : 'black';
 
   return (
@@ -25,11 +29,13 @@ const Day = ({ variant = 'thisMonth', day, dayOfWeek, event }: DayProps) => {
         {day}
       </div>
       {event ? (
-        <p className='font-bold-18 h-full overflow-auto'>{event}</p>
+        <p className='font-bold-18 h-full w-full overflow-auto'>{event}</p>
       ) : (
-        <div className='flex h-full w-full flex-col items-center justify-center'>
-          <PlusButton variant={variant === 'anotherMonth' ? 'light' : 'dark'} />
-        </div>
+        !isPast && (
+          <div className='flex h-full w-full flex-col items-center justify-center'>
+            <PlusButton variant={variant === 'anotherMonth' ? 'light' : 'dark'} />
+          </div>
+        )
       )}
     </div>
   );
@@ -40,6 +46,7 @@ const Calendar = () => {
 
   const handleClickPrevMonth = () => setCurrentMonth(currentMonth.subtract(1, 'month'));
   const handleClickNextMonth = () => setCurrentMonth(currentMonth.add(1, 'month'));
+  const handleClickCurrentMonth = () => setCurrentMonth(dayjs().startOf('month'));
 
   const daysInMonth = currentMonth.daysInMonth();
   const firstDayOfMonth = currentMonth.startOf('month').day(); // 1일 요일, 0: Sunday ~ 6: Saturday
@@ -47,9 +54,12 @@ const Calendar = () => {
   const daysArray = Array.from({ length: daysInMonth }, (_, i) => i + 1);
 
   const prevMonth = currentMonth.subtract(1, 'month');
+  const nextMonth = currentMonth.add(1, 'month');
   const daysInPrevMonth = prevMonth.daysInMonth();
 
+  // 삭제예정
   const publishData = [
+    { date: '2024-12-20', text: '점과 직선' },
     { date: '2025-03-02', text: '점과 직선 사이의 거리 돌아보기 점과 직선 사이의 거리 돌아보기' },
     { date: '2025-03-08', text: '점과 직선 사이의 거리 돌아보기' },
   ];
@@ -58,7 +68,9 @@ const Calendar = () => {
     <div className='w-full'>
       <div className='mb-[7.4rem] flex items-center justify-center gap-[4.8rem]'>
         <IconButton variant='left' onClick={handleClickPrevMonth} />
-        <h2 className='font-bold-32'>{currentMonth.format('YYYY년 M월')}</h2>
+        <h2 className='font-bold-32 cursor-pointer' onClick={handleClickCurrentMonth}>
+          {currentMonth.format('YYYY년 M월')}
+        </h2>
         <IconButton variant='right' onClick={handleClickNextMonth} />
       </div>
 
@@ -78,6 +90,9 @@ const Calendar = () => {
             key={`prev-${i}`}
             variant='anotherMonth'
             day={daysInPrevMonth - firstDayOfMonth + i + 1}
+            fullDate={prevMonth
+              .date(daysInPrevMonth - firstDayOfMonth + i + 1)
+              .format('YYYY-MM-DD')}
           />
         ))}
 
@@ -85,11 +100,24 @@ const Calendar = () => {
           const fullDate = currentMonth.date(day).format('YYYY-MM-DD');
           const event = publishData.find((e) => e.date === fullDate);
           const dayOfWeek = currentMonth.date(day).day();
-          return <Day key={day} day={day} dayOfWeek={dayOfWeek} event={event?.text} />;
+          return (
+            <Day
+              key={day}
+              day={day}
+              dayOfWeek={dayOfWeek}
+              event={event?.text}
+              fullDate={fullDate}
+            />
+          );
         })}
 
         {Array.from({ length: 6 - lastDayOfMonth }).map((_, i) => (
-          <Day key={`next-${i}`} variant='anotherMonth' day={i + 1} />
+          <Day
+            key={`next-${i}`}
+            variant='anotherMonth'
+            day={i + 1}
+            fullDate={nextMonth.date(i + 1).format('YYYY-MM-DD')}
+          />
         ))}
       </div>
     </div>
