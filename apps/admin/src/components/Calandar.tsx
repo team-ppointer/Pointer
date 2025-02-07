f6624c8f6bdf51ce13f17d7cc87e0de9c97f8409
Yoo TaeSeung <remicon99@gmail.com>
2025-02-07 19:28:01 +0900

    fix: 캘린더 수정 (event -> title, 지난달/다음달 title 미출력 오류 수정)

--- apps/admin/src/components/Calandar.tsx
+++ apps/admin/src/components/Calandar.tsx
@@ -12,10 +12,10 @@ interface DayProps extends HTMLAttributes<HTMLDivElement> {
   fullDate: string;
   day: number;
   dayOfWeek?: number; // 요일 - 0: Sunday ~ 6: Saturday
-  event?: string;
+  title?: string;
 }
 
-const Day = ({ variant = 'thisMonth', fullDate, day, dayOfWeek, event }: DayProps) => {
+const Day = ({ variant = 'thisMonth', fullDate, day, dayOfWeek, title }: DayProps) => {
   const today = dayjs().startOf('day');
   const isPast = dayjs(fullDate).isBefore(today, 'day');
   const isToday = dayjs(fullDate).isSame(today, 'day');
@@ -24,7 +24,9 @@ const Day = ({ variant = 'thisMonth', fullDate, day, dayOfWeek, event }: DayProp
 
   const dayOfWeekStyle = isSunday ? 'text-red' : isSaturday ? 'text-blue' : 'black';
 
-  const bgStyle =
+  const textStyle = variant === 'thisMonth' ? 'text-black' : 'text-lightgray500';
+
+  const todayBgStyle =
     isToday && variant === 'thisMonth'
       ? isSunday
         ? 'bg-lightred'
@@ -35,13 +37,13 @@ const Day = ({ variant = 'thisMonth', fullDate, day, dayOfWeek, event }: DayProp
 
   return (
     <div
-      className={`flex h-[15rem] flex-col items-end gap-[0.4rem] border bg-white px-[2.4rem] py-[1.6rem] text-black`}>
+      className={`flex h-[15rem] flex-col items-end gap-[0.4rem] border bg-white px-[2.4rem] py-[1.6rem]`}>
       <div
-        className={`font-medium-16 h-[2.4rem] rounded-[0.4rem] px-[0.6rem] text-end ${dayOfWeekStyle} ${variant === 'anotherMonth' && 'text-lightgray500'} ${bgStyle}`}>
+        className={`font-medium-16 h-[2.4rem] rounded-[0.4rem] px-[0.6rem] text-end ${dayOfWeekStyle} ${textStyle} ${todayBgStyle}`}>
         {day}
       </div>
-      {event ? (
-        <p className='font-bold-18 h-full w-full overflow-auto'>{event}</p>
+      {title ? (
+        <p className={`font-bold-18 h-full w-full overflow-auto ${textStyle}`}>{title}</p>
       ) : (
         !isPast && (
           <div className='flex h-full w-full flex-col items-center justify-center'>
@@ -71,8 +73,8 @@ const Calendar = () => {
 
   // 삭제예정
   const publishData = [
-    { date: '2024-12-20', text: '점과 직선' },
-    { date: '2025-03-02', text: '점과 직선 사이의 거리 돌아보기 점과 직선 사이의 거리 돌아보기' },
+    { date: '2024-12-30', text: '점과 직선' },
+    { date: '2025-03-01', text: '점과 직선 사이의 거리 돌아보기 점과 직선 사이의 거리 돌아보기' },
     { date: '2025-03-08', text: '점과 직선 사이의 거리 돌아보기' },
   ];
 
@@ -97,40 +99,52 @@ const Calendar = () => {
       </div>
 
       <div className='grid grid-cols-7 gap-[0.8rem]'>
-        {Array.from({ length: firstDayOfMonth }).map((_, i) => (
-          <Day
-            key={`prev-${i}`}
-            variant='anotherMonth'
-            day={daysInPrevMonth - firstDayOfMonth + i + 1}
-            fullDate={prevMonth
-              .date(daysInPrevMonth - firstDayOfMonth + i + 1)
-              .format('YYYY-MM-DD')}
-          />
-        ))}
+        {Array.from({ length: firstDayOfMonth }).map((_, i) => {
+          const fullDate = prevMonth
+            .date(daysInPrevMonth - firstDayOfMonth + i + 1)
+            .format('YYYY-MM-DD');
+          const title = publishData.find((e) => e.date === fullDate);
+
+          return (
+            <Day
+              key={`prev-${i}`}
+              variant='anotherMonth'
+              day={daysInPrevMonth - firstDayOfMonth + i + 1}
+              fullDate={fullDate}
+              title={title?.text}
+            />
+          );
+        })}
 
         {daysArray.map((day) => {
           const fullDate = currentMonth.date(day).format('YYYY-MM-DD');
-          const event = publishData.find((e) => e.date === fullDate);
+          const title = publishData.find((e) => e.date === fullDate);
           const dayOfWeek = currentMonth.date(day).day();
           return (
             <Day
               key={day}
               day={day}
               dayOfWeek={dayOfWeek}
-              event={event?.text}
+              title={title?.text}
               fullDate={fullDate}
             />
           );
         })}
 
-        {Array.from({ length: 6 - lastDayOfMonth }).map((_, i) => (
-          <Day
-            key={`next-${i}`}
-            variant='anotherMonth'
-            day={i + 1}
-            fullDate={nextMonth.date(i + 1).format('YYYY-MM-DD')}
-          />
-        ))}
+        {Array.from({ length: 6 - lastDayOfMonth }).map((_, i) => {
+          const fullDate = nextMonth.date(i + 1).format('YYYY-MM-DD');
+          const title = publishData.find((e) => e.date === fullDate);
+
+          return (
+            <Day
+              key={`next-${i}`}
+              variant='anotherMonth'
+              day={i + 1}
+              fullDate={fullDate}
+              title={title?.text}
+            />
+          );
+        })}
       </div>
     </div>
   );
