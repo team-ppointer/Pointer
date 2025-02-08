000e2f8f62b3f73817672f4930f38cceffc8c238
Yoo TaeSeung <remicon99@gmail.com>
2025-02-08 14:11:21 +0900

    fix: 컴포넌트 줄바꿈 방지

--- apps/admin/src/components/Buttons/DeleteButton.tsx
+++ apps/admin/src/components/Buttons/DeleteButton.tsx
@@ -7,7 +7,7 @@ interface DeleteButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 const DeleteButton = ({ label }: DeleteButtonProps) => {
   return (
-    <button className='bg-lightred flex h-[4.3rem] w-[13.1rem] min-w-[13.1rem] items-center justify-center gap-[0.8rem] rounded-[0.8rem] px-[1.6rem] py-[0.95rem]'>
+    <button className='bg-lightred flex h-[4.3rem] w-[13.1rem] items-center justify-center gap-[0.8rem] rounded-[0.8rem] px-[1.6rem] py-[0.95rem] whitespace-nowrap'>
       <IcDelete width={24} height={24} />
       <span className='font-medium-18 text-red'>{label}</span>
     </button>
