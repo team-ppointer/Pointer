d58291d461037fc1a1ad80b50a9bffa0e03c6337
Yoo TaeSeung <remicon99@gmail.com>
2025-02-12 03:22:08 +0900

    feat: 문항 등록 페이지 퍼블리싱 중

--- apps/admin/src/routes/_GNBLayout/problem/$problemId/index.tsx
+++ apps/admin/src/routes/_GNBLayout/problem/$problemId/index.tsx
@@ -1,9 +1,32 @@
+import { Header } from '@components';
 import { createFileRoute } from '@tanstack/react-router';
+import { ReactNode } from 'react';
 
 export const Route = createFileRoute('/_GNBLayout/problem/$problemId/')({
   component: RouteComponent,
 });
 
+const SectionLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <section className='border-lightgray500 w-full rounded-[1.6rem] border bg-white p-[3.2rem]'>
+      {children}
+    </section>
+  );
+};
+
 function RouteComponent() {
-  return <div>{`Hello "/problem/$problemId/"!`}</div>;
+  const { problemId } = Route.useParams();
+  return (
+    <>
+      <Header title={`문항 ID : ${problemId}`} />
+      <SectionLayout>
+        <div className='flex items-baseline gap-[1.6rem]'>
+          <h3 className='font-bold-32 text-black'>새끼 문제 등록</h3>
+          <p className='font-medium-14 text-lightgray500'>
+            새끼 문항은 저장 후 항목 추가가 불가능해요
+          </p>
+        </div>
+      </SectionLayout>
+    </>
+  );
 }
