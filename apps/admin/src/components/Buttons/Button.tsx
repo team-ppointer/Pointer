e33d13958ea6102020465ecf8f778c4c3a36c275
Yoo TaeSeung <remicon99@gmail.com>
2025-02-12 22:51:06 +0900

    design: Button 컴포넌트 스타일 수정

--- apps/admin/src/components/Buttons/Button.tsx
+++ apps/admin/src/components/Buttons/Button.tsx
@@ -17,15 +17,15 @@ const Button = ({
   const baseStyles = 'rounded-[8px] font-medium-18 flex items-center justify-center';
 
   const sizeStyles = {
-    short: 'w-[12rem] h-[5.6rem]',
-    long: 'w-[34.8rem] h-[5.6rem]',
+    short: 'min-w-[12rem] w-[12rem] h-[5.6rem]',
+    long: 'min-w-[34.8rem] w-[34.8rem] h-[5.6rem]',
   };
 
   const variantStyles = {
     blue: 'bg-blue text-white',
     dark: 'bg-darkgray100 text-white',
     light: 'bg-white text-black border border-lightgray500',
-    dimmed: 'bg-transparent text-lightgray5',
+    dimmed: 'bg-transparent text-lightgray500',
   };
 
   return (
