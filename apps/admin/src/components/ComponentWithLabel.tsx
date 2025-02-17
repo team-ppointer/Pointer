c1cf770dd95f4d9ab01fb473096feda9bf2b77f7
Yoo TaeSeung <remicon99@gmail.com>
2025-02-18 01:14:13 +0900

    fix: ComponentWithLabel 동적 너비 지정

--- apps/admin/src/components/ComponentWithLabel.tsx
+++ apps/admin/src/components/ComponentWithLabel.tsx
@@ -9,7 +9,11 @@ interface ComponentWithLabelProps {
 const ComponentWithLabel = ({ label, labelWidth, children }: ComponentWithLabelProps) => {
   return (
     <div className='flex w-full items-center gap-[2.4rem]'>
-      <h6 className={`font-medium-18 whitespace-nowrap ${labelWidth}`}>{label}</h6>
+      <h6
+        className='font-medium-18 whitespace-nowrap'
+        style={{ width: labelWidth, minWidth: labelWidth }}>
+        {label}
+      </h6>
       {children}
     </div>
   );
