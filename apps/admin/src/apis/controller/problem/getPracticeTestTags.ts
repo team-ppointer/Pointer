18b9e473b643610e73dddf386ab6ad180e536516
Yoo TaeSeung <remicon99@gmail.com>
2025-02-17 17:02:50 +0900

    feat: 모의고사 GET API staleTime, gcTime 설정

--- apps/admin/src/apis/controller/problem/getPracticeTestTags.ts
+++ apps/admin/src/apis/controller/problem/getPracticeTestTags.ts
@@ -1,7 +1,15 @@
 import { $api } from 'src/apis/client';
 
 const getPracticeTestTags = () => {
-  return $api.useQuery('get', '/api/v1/practiceTestTags');
+  return $api.useQuery(
+    'get',
+    '/api/v1/practiceTestTags',
+    {},
+    {
+      staleTime: Infinity,
+      gcTime: Infinity,
+    }
+  );
 };
 
 export default getPracticeTestTags;
