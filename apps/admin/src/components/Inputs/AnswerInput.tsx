5e374c9e1dbed10029670fc0cf261007ac28e51d
Yoo TaeSeung <remicon99@gmail.com>
2025-02-18 15:51:43 +0900

    feat: AnswerInput 폼 작업 (RHF 연결을 위한 리팩토링)

--- apps/admin/src/components/Inputs/AnswerInput.tsx
+++ apps/admin/src/components/Inputs/AnswerInput.tsx
@@ -1,96 +1,74 @@
+import { Input } from '@components';
 import { ProblemAnswerType } from '@types';
+import { forwardRef } from 'react';
 
-interface ProblemAnswerTypeButtonProps {
-  label: string;
-  isSelected: boolean;
-  onClick: () => void;
-}
-
-interface NumberProps {
-  label: number;
-  isSelected: boolean;
-  onClick: () => void;
-}
-
-interface AnswerInputProps {
-  problemAnswerType: ProblemAnswerType;
-  answer: string | null;
-  handleClickProblemAnswerType: (type: ProblemAnswerType) => void;
-  handleChangeAnswer: (value: string) => void;
-}
-
-const ProblemAnswerTypeButton = ({ label, isSelected, onClick }: ProblemAnswerTypeButtonProps) => {
-  const selectedStyle = isSelected
-    ? 'bg-midgray200 text-white'
-    : 'bg-lightgray300 text-lightgray500';
-
-  return (
-    <div
-      className={`font-medium-24 flex h-[5.6rem] w-[5.6rem] items-center justify-center ${selectedStyle} cursor-pointer rounded-[4px]`}
-      onClick={onClick}>
-      {label}
-    </div>
-  );
+const AnswerTypeList = ['MULTIPLE_CHOICE', 'SHORT_STRING_ANSWER'];
+const AnswerTypeName = {
+  MULTIPLE_CHOICE: '객',
+  SHORT_STRING_ANSWER: '주',
 };
 
-const Input = () => {
-  return (
-    <input
-      className='placeholder:text-lightgray500 font-bold-24 border-lightgray500 h-[5.6rem] w-full rounded-[16px] border bg-white px-[1.6rem] text-black'
-      placeholder='입력해주세요'
-    />
-  );
+interface AnswerTypeSectionProps {
+  selectedAnswerType: ProblemAnswerType | undefined;
+}
+
+interface AnswerInputSectionProps {
+  selectedAnswerType: ProblemAnswerType | undefined;
+  selectedAnswer: string | undefined;
+}
+
+const AnswerInput = ({ children }: { children: React.ReactNode }) => {
+  return <div className='flex w-full items-center gap-[2.4rem]'>{children}</div>;
 };
 
-const Number = ({ label, isSelected, onClick }: NumberProps) => {
-  const selectedStyle = isSelected
-    ? 'bg-darkgray100 text-white'
-    : 'bg-white text-black border border-lightgray500';
-
-  return (
-    <div
-      className={`font-bold-24 flex h-[5.6rem] w-[5.6rem] cursor-pointer items-center justify-center rounded-full ${selectedStyle}`}
-      onClick={onClick}>
-      {label}
-    </div>
-  );
-};
-
-const AnswerInput = ({
-  problemAnswerType,
-  answer,
-  handleClickProblemAnswerType,
-  handleChangeAnswer,
-}: AnswerInputProps) => {
-  return (
-    <div className='flex min-w-[44rem] gap-[2.4rem]'>
+const AnswerTypeSection = forwardRef<HTMLInputElement, AnswerTypeSectionProps>(
+  ({ selectedAnswerType = 'MULTIPLE_CHOICE', ...props }, ref) => {
+    return (
       <div className='flex gap-[0.8rem]'>
-        <ProblemAnswerTypeButton
-          label='객'
-          isSelected={problemAnswerType === 'MULTIPLE_CHOICE'}
-          onClick={() => handleClickProblemAnswerType('MULTIPLE_CHOICE')}
-        />
-        <ProblemAnswerTypeButton
-          label='주'
-          isSelected={problemAnswerType === 'SINGLE_CHOICE'}
-          onClick={() => handleClickProblemAnswerType('SINGLE_CHOICE')}
-        />
+        {AnswerTypeList.map((answerType) => (
+          <label key={answerType}>
+            <input type='radio' className='hidden' ref={ref} value={answerType} {...props} />
+            <div
+              className={`flex h-[5.6rem] w-[5.6rem] cursor-pointer items-center justify-center rounded-[4px] ${answerType === selectedAnswerType ? 'bg-midgray200 text-white' : 'bg-lightgray300 text-lightgray500'} `}>
+              <span className='font-medium-24'>
+                {AnswerTypeName[answerType as ProblemAnswerType]}
+              </span>
+            </div>
+          </label>
+        ))}
       </div>
-      {problemAnswerType === 'SINGLE_CHOICE' && <Input />}
-      {problemAnswerType === 'MULTIPLE_CHOICE' && (
-        <div className='flex items-center justify-between gap-[1.6rem]'>
-          {[1, 2, 3, 4, 5].map((num) => (
-            <Number
-              key={num}
-              label={num}
-              isSelected={answer === num.toString()}
-              onClick={() => handleChangeAnswer(num.toString())}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
-};
+    );
+  }
+);
+
+const AnswerInputSection = forwardRef<HTMLInputElement, AnswerInputSectionProps>(
+  ({ selectedAnswerType, selectedAnswer, ...props }, ref) => {
+    return (
+      <>
+        {selectedAnswerType === 'SHORT_STRING_ANSWER' && <Input ref={ref} {...props} />}
+        {selectedAnswerType === 'MULTIPLE_CHOICE' && (
+          <div className='flex items-center justify-between gap-[1.6rem]'>
+            {Array.from({ length: 5 }, (_, i) => (i + 1).toString()).map((num) => (
+              <label key={num}>
+                <input type='radio' className='hidden' value={num} ref={ref} {...props} />
+                <div
+                  className={`flex h-[5.6rem] w-[5.6rem] cursor-pointer items-center justify-center rounded-full ${
+                    selectedAnswer === num
+                      ? 'bg-darkgray100 text-white'
+                      : 'border-lightgray500 border bg-white text-black'
+                  }`}>
+                  <span className='font-bold-24'>{num}</span>
+                </div>
+              </label>
+            ))}
+          </div>
+        )}
+      </>
+    );
+  }
+);
+
+AnswerInput.AnswerTypeSection = AnswerTypeSection;
+AnswerInput.AnswerInputSection = AnswerInputSection;
 
 export default AnswerInput;
--- apps/admin/src/types/component.ts
+++ apps/admin/src/types/component.ts
@@ -4,7 +4,7 @@ export type ExamType = { id: number; name: string };
 
 export type ProblemType = 'CREATION_PROBLEM' | 'GICHUL_PROBLEM' | 'VARIANT_PROBLEM';
 
-export type ProblemAnswerType = 'MULTIPLE_CHOICE' | 'SINGLE_CHOICE';
+export type ProblemAnswerType = 'MULTIPLE_CHOICE' | 'SHORT_STRING_ANSWER';
 
 export type LevelType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
