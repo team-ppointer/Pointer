a688955ad731642e0add09d1ec0cc0bcf208794f
Yoo TaeSeung <remicon99@gmail.com>
2025-02-20 23:46:31 +0900

    feat: problem search - staleTime, gcTime 설정

--- apps/admin/src/apis/controller/problem/getProblemsSearch.ts
+++ apps/admin/src/apis/controller/problem/getProblemsSearch.ts
@@ -2,11 +2,19 @@ import { $api } from '@apis';
 import { getProblemsSearchParamsType } from '@types';
 
 const getProblemsSearch = (search: getProblemsSearchParamsType) => {
-  return $api.useQuery('get', '/api/v1/problems/search', {
-    params: {
-      query: search,
+  return $api.useQuery(
+    'get',
+    '/api/v1/problems/search',
+    {
+      params: {
+        query: search,
+      },
     },
-  });
+    {
+      staleTime: 1000 * 60 * 60,
+      gcTime: 1000 * 60 * 60 * 24,
+    }
+  );
 };
 
 export default getProblemsSearch;
