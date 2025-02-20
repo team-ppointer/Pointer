86a5fe8d85136dc9c5fdbf73f283b719bfa8d71b
Yoo TaeSeung <remicon99@gmail.com>
2025-02-20 22:12:32 +0900

    feat: api 유틸 함수 제작

--- apps/admin/src/utils/api.ts
+++ apps/admin/src/utils/api.ts
@@ -1,6 +1,43 @@
+import { components } from '@schema';
 import { TagType } from '@types';
+
+type ProblemGetResponse = components['schemas']['ProblemGetResponse'];
+type ProblemUpdateRequest = components['schemas']['ProblemUpdateRequest'];
 
 export const tagToQueryParams = (tags: TagType[]) => {
   if (!tags.length) return {};
   return { conceptTagIds: tags.map((tag) => tag.id) };
+};
+
+export const transformToProblemUpdateRequest = (
+  serverData: ProblemGetResponse
+): ProblemUpdateRequest => {
+  return {
+    problemType: serverData.problemType ?? 'GICHUL_PROBLEM',
+    practiceTestId: serverData.practiceTestId,
+    number: serverData.number,
+    conceptTagIds: serverData.conceptTagIds ?? [],
+    answer: serverData.answer,
+    title: serverData.title,
+    difficulty: serverData.difficulty,
+    memo: serverData.memo ?? '',
+    mainProblemImageUrl: serverData.mainProblemImageUrl,
+    mainAnalysisImageUrl: serverData.mainAnalysisImageUrl,
+    mainHandwritingExplanationImageUrl: serverData.mainHandwritingExplanationImageUrl,
+    readingTipImageUrl: serverData.readingTipImageUrl,
+    seniorTipImageUrl: serverData.seniorTipImageUrl,
+    prescriptionImageUrls: serverData.prescriptionImageUrls ?? [],
+
+    answerType: serverData.answerType,
+    recommendedMinute: serverData.recommendedMinute,
+    recommendedSecond: serverData.recommendedSecond,
+
+    updateChildProblems: (serverData.childProblems || []).map((child) => ({
+      childProblemId: child.childProblemId, 
+      conceptTagIds: [...child.conceptTagIds],
+      imageUrl: child.imageUrl ?? '',
+      answerType: child.answerType,
+      answer: child.answer,
+    })),
+  };
 };
