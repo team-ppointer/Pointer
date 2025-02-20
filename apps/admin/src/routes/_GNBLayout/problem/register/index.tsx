94220f4de15b94cf26f4a9cfa3bd0415c0788052
Yoo TaeSeung <remicon99@gmail.com>
2025-02-20 22:13:14 +0900

    feat: 문항 등록 유효성 검사 & 에러 처리

--- apps/admin/src/routes/_GNBLayout/problem/register/index.tsx
+++ apps/admin/src/routes/_GNBLayout/problem/register/index.tsx
@@ -1,24 +1,39 @@
 import { postProblems } from '@apis';
 import { Button, Header, ProblemEssentialInput } from '@components';
 import { createFileRoute, useRouter } from '@tanstack/react-router';
-import { ProblemType } from '@types';
+import { problemTypeSchema } from '@types';
 import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { components } from '@schema';
 
 export const Route = createFileRoute('/_GNBLayout/problem/register/')({
   component: RouteComponent,
 });
 
-type EssentialInput = {
-  problemType: ProblemType;
-  practiceTestId: number | undefined;
-  number: number | undefined;
-};
+type ProblemPostRequest = components['schemas']['ProblemPostRequest'];
+
+const essentialInputSchema = z.object({
+  problemType: problemTypeSchema,
+  practiceTestId: z.number().optional(),
+  number: z.number().optional(),
+});
 
 function RouteComponent() {
   const { navigate } = useRouter();
 
   const { mutate } = postProblems();
-  const { control, register, setValue, watch, handleSubmit } = useForm<EssentialInput>({
+  const {
+    control,
+    register,
+    setValue,
+    watch,
+    handleSubmit,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(essentialInputSchema),
     defaultValues: {
       problemType: 'GICHUL_PROBLEM',
       practiceTestId: undefined,
@@ -27,7 +42,17 @@ function RouteComponent() {
   });
   const problemType = watch('problemType');
 
-  const handleClickRegister = (data: EssentialInput) => {
+  const handleClickRegister = (data: ProblemPostRequest) => {
+    if (data.problemType !== 'CREATION_PROBLEM' && (!data.practiceTestId || !data.number)) {
+      setError('practiceTestId', {
+        message: '모의고사와 문항 번호는 필수 입력 항목입니다.',
+      });
+      setError('number', {
+        message: '모의고사와 문항 번호는 필수 입력 항목입니다.',
+      });
+      return;
+    }
+
     mutate(
       {
         body: data,
@@ -42,6 +67,7 @@ function RouteComponent() {
 
     return;
   };
+
   return (
     <>
       <Header title='문항 등록' />
@@ -58,6 +84,7 @@ function RouteComponent() {
                     setValue('practiceTestId', undefined);
                     setValue('number', undefined);
                   }
+                  clearErrors();
                   field.onChange(type);
                 }}
               />
@@ -79,6 +106,11 @@ function RouteComponent() {
                 {...register('number', { valueAsNumber: true })}
               />
             </ProblemEssentialInput.PracticeTestSection>
+          )}
+          {(errors.practiceTestId || errors.number) && (
+            <p className='text-red font-medium-18 mt-[2rem]'>
+              모의고사와 문항 번호는 필수 입력 항목입니다.
+            </p>
           )}
         </ProblemEssentialInput>
         <div className='mt-[2.4rem] flex w-full items-center justify-end'>
