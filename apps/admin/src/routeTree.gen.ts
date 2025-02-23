db6eef017d53dfb44e52d65865e13bda0057dce3
Yoo TaeSeung <remicon99@gmail.com>
2025-02-24 04:15:07 +0900

    style: 코드 포맷팅

--- apps/admin/src/routeTree.gen.ts
+++ apps/admin/src/routeTree.gen.ts
@@ -10,185 +10,184 @@
 
 // Import Routes
 
-import { Route as rootRoute } from './routes/__root'
-import { Route as GNBLayoutImport } from './routes/_GNBLayout'
-import { Route as IndexImport } from './routes/index'
-import { Route as LoginIndexImport } from './routes/login/index'
-import { Route as GNBLayoutPublishIndexImport } from './routes/_GNBLayout/publish/index'
-import { Route as GNBLayoutProblemIndexImport } from './routes/_GNBLayout/problem/index'
-import { Route as GNBLayoutProblemSetIndexImport } from './routes/_GNBLayout/problem-set/index'
-import { Route as GNBLayoutComponentIndexImport } from './routes/_GNBLayout/component/index'
-import { Route as GNBLayoutProblemRegisterIndexImport } from './routes/_GNBLayout/problem/register/index'
-import { Route as GNBLayoutProblemProblemIdIndexImport } from './routes/_GNBLayout/problem/$problemId/index'
-import { Route as GNBLayoutProblemSetProblemSetIdIndexImport } from './routes/_GNBLayout/problem-set/$problemSetId/index'
-import { Route as GNBLayoutPublishRegisterPublishDateIndexImport } from './routes/_GNBLayout/publish/register/$publishDate/index'
+import { Route as rootRoute } from './routes/__root';
+import { Route as GNBLayoutImport } from './routes/_GNBLayout';
+import { Route as IndexImport } from './routes/index';
+import { Route as LoginIndexImport } from './routes/login/index';
+import { Route as GNBLayoutPublishIndexImport } from './routes/_GNBLayout/publish/index';
+import { Route as GNBLayoutProblemIndexImport } from './routes/_GNBLayout/problem/index';
+import { Route as GNBLayoutProblemSetIndexImport } from './routes/_GNBLayout/problem-set/index';
+import { Route as GNBLayoutComponentIndexImport } from './routes/_GNBLayout/component/index';
+import { Route as GNBLayoutProblemRegisterIndexImport } from './routes/_GNBLayout/problem/register/index';
+import { Route as GNBLayoutProblemProblemIdIndexImport } from './routes/_GNBLayout/problem/$problemId/index';
+import { Route as GNBLayoutProblemSetProblemSetIdIndexImport } from './routes/_GNBLayout/problem-set/$problemSetId/index';
+import { Route as GNBLayoutPublishRegisterPublishDateIndexImport } from './routes/_GNBLayout/publish/register/$publishDate/index';
 
 // Create/Update Routes
 
 const GNBLayoutRoute = GNBLayoutImport.update({
   id: '/_GNBLayout',
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const IndexRoute = IndexImport.update({
   id: '/',
   path: '/',
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const LoginIndexRoute = LoginIndexImport.update({
   id: '/login/',
   path: '/login/',
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const GNBLayoutPublishIndexRoute = GNBLayoutPublishIndexImport.update({
   id: '/publish/',
   path: '/publish/',
   getParentRoute: () => GNBLayoutRoute,
-} as any)
+} as any);
 
 const GNBLayoutProblemIndexRoute = GNBLayoutProblemIndexImport.update({
   id: '/problem/',
   path: '/problem/',
   getParentRoute: () => GNBLayoutRoute,
-} as any)
+} as any);
 
 const GNBLayoutProblemSetIndexRoute = GNBLayoutProblemSetIndexImport.update({
   id: '/problem-set/',
   path: '/problem-set/',
   getParentRoute: () => GNBLayoutRoute,
-} as any)
+} as any);
 
 const GNBLayoutComponentIndexRoute = GNBLayoutComponentIndexImport.update({
   id: '/component/',
   path: '/component/',
   getParentRoute: () => GNBLayoutRoute,
-} as any)
+} as any);
 
-const GNBLayoutProblemRegisterIndexRoute =
-  GNBLayoutProblemRegisterIndexImport.update({
-    id: '/problem/register/',
-    path: '/problem/register/',
-    getParentRoute: () => GNBLayoutRoute,
-  } as any)
+const GNBLayoutProblemRegisterIndexRoute = GNBLayoutProblemRegisterIndexImport.update({
+  id: '/problem/register/',
+  path: '/problem/register/',
+  getParentRoute: () => GNBLayoutRoute,
+} as any);
 
-const GNBLayoutProblemProblemIdIndexRoute =
-  GNBLayoutProblemProblemIdIndexImport.update({
-    id: '/problem/$problemId/',
-    path: '/problem/$problemId/',
-    getParentRoute: () => GNBLayoutRoute,
-  } as any)
+const GNBLayoutProblemProblemIdIndexRoute = GNBLayoutProblemProblemIdIndexImport.update({
+  id: '/problem/$problemId/',
+  path: '/problem/$problemId/',
+  getParentRoute: () => GNBLayoutRoute,
+} as any);
 
-const GNBLayoutProblemSetProblemSetIdIndexRoute =
-  GNBLayoutProblemSetProblemSetIdIndexImport.update({
+const GNBLayoutProblemSetProblemSetIdIndexRoute = GNBLayoutProblemSetProblemSetIdIndexImport.update(
+  {
     id: '/problem-set/$problemSetId/',
     path: '/problem-set/$problemSetId/',
     getParentRoute: () => GNBLayoutRoute,
-  } as any)
+  } as any
+);
 
 const GNBLayoutPublishRegisterPublishDateIndexRoute =
   GNBLayoutPublishRegisterPublishDateIndexImport.update({
     id: '/publish/register/$publishDate/',
     path: '/publish/register/$publishDate/',
     getParentRoute: () => GNBLayoutRoute,
-  } as any)
+  } as any);
 
 // Populate the FileRoutesByPath interface
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
     '/': {
-      id: '/'
-      path: '/'
-      fullPath: '/'
-      preLoaderRoute: typeof IndexImport
-      parentRoute: typeof rootRoute
-    }
+      id: '/';
+      path: '/';
+      fullPath: '/';
+      preLoaderRoute: typeof IndexImport;
+      parentRoute: typeof rootRoute;
+    };
     '/_GNBLayout': {
-      id: '/_GNBLayout'
-      path: ''
-      fullPath: ''
-      preLoaderRoute: typeof GNBLayoutImport
-      parentRoute: typeof rootRoute
-    }
+      id: '/_GNBLayout';
+      path: '';
+      fullPath: '';
+      preLoaderRoute: typeof GNBLayoutImport;
+      parentRoute: typeof rootRoute;
+    };
     '/login/': {
-      id: '/login/'
-      path: '/login'
-      fullPath: '/login'
-      preLoaderRoute: typeof LoginIndexImport
-      parentRoute: typeof rootRoute
-    }
+      id: '/login/';
+      path: '/login';
+      fullPath: '/login';
+      preLoaderRoute: typeof LoginIndexImport;
+      parentRoute: typeof rootRoute;
+    };
     '/_GNBLayout/component/': {
-      id: '/_GNBLayout/component/'
-      path: '/component'
-      fullPath: '/component'
-      preLoaderRoute: typeof GNBLayoutComponentIndexImport
-      parentRoute: typeof GNBLayoutImport
-    }
+      id: '/_GNBLayout/component/';
+      path: '/component';
+      fullPath: '/component';
+      preLoaderRoute: typeof GNBLayoutComponentIndexImport;
+      parentRoute: typeof GNBLayoutImport;
+    };
     '/_GNBLayout/problem-set/': {
-      id: '/_GNBLayout/problem-set/'
-      path: '/problem-set'
-      fullPath: '/problem-set'
-      preLoaderRoute: typeof GNBLayoutProblemSetIndexImport
-      parentRoute: typeof GNBLayoutImport
-    }
+      id: '/_GNBLayout/problem-set/';
+      path: '/problem-set';
+      fullPath: '/problem-set';
+      preLoaderRoute: typeof GNBLayoutProblemSetIndexImport;
+      parentRoute: typeof GNBLayoutImport;
+    };
     '/_GNBLayout/problem/': {
-      id: '/_GNBLayout/problem/'
-      path: '/problem'
-      fullPath: '/problem'
-      preLoaderRoute: typeof GNBLayoutProblemIndexImport
-      parentRoute: typeof GNBLayoutImport
-    }
+      id: '/_GNBLayout/problem/';
+      path: '/problem';
+      fullPath: '/problem';
+      preLoaderRoute: typeof GNBLayoutProblemIndexImport;
+      parentRoute: typeof GNBLayoutImport;
+    };
     '/_GNBLayout/publish/': {
-      id: '/_GNBLayout/publish/'
-      path: '/publish'
-      fullPath: '/publish'
-      preLoaderRoute: typeof GNBLayoutPublishIndexImport
-      parentRoute: typeof GNBLayoutImport
-    }
+      id: '/_GNBLayout/publish/';
+      path: '/publish';
+      fullPath: '/publish';
+      preLoaderRoute: typeof GNBLayoutPublishIndexImport;
+      parentRoute: typeof GNBLayoutImport;
+    };
     '/_GNBLayout/problem-set/$problemSetId/': {
-      id: '/_GNBLayout/problem-set/$problemSetId/'
-      path: '/problem-set/$problemSetId'
-      fullPath: '/problem-set/$problemSetId'
-      preLoaderRoute: typeof GNBLayoutProblemSetProblemSetIdIndexImport
-      parentRoute: typeof GNBLayoutImport
-    }
+      id: '/_GNBLayout/problem-set/$problemSetId/';
+      path: '/problem-set/$problemSetId';
+      fullPath: '/problem-set/$problemSetId';
+      preLoaderRoute: typeof GNBLayoutProblemSetProblemSetIdIndexImport;
+      parentRoute: typeof GNBLayoutImport;
+    };
     '/_GNBLayout/problem/$problemId/': {
-      id: '/_GNBLayout/problem/$problemId/'
-      path: '/problem/$problemId'
-      fullPath: '/problem/$problemId'
-      preLoaderRoute: typeof GNBLayoutProblemProblemIdIndexImport
-      parentRoute: typeof GNBLayoutImport
-    }
+      id: '/_GNBLayout/problem/$problemId/';
+      path: '/problem/$problemId';
+      fullPath: '/problem/$problemId';
+      preLoaderRoute: typeof GNBLayoutProblemProblemIdIndexImport;
+      parentRoute: typeof GNBLayoutImport;
+    };
     '/_GNBLayout/problem/register/': {
-      id: '/_GNBLayout/problem/register/'
-      path: '/problem/register'
-      fullPath: '/problem/register'
-      preLoaderRoute: typeof GNBLayoutProblemRegisterIndexImport
-      parentRoute: typeof GNBLayoutImport
-    }
+      id: '/_GNBLayout/problem/register/';
+      path: '/problem/register';
+      fullPath: '/problem/register';
+      preLoaderRoute: typeof GNBLayoutProblemRegisterIndexImport;
+      parentRoute: typeof GNBLayoutImport;
+    };
     '/_GNBLayout/publish/register/$publishDate/': {
-      id: '/_GNBLayout/publish/register/$publishDate/'
-      path: '/publish/register/$publishDate'
-      fullPath: '/publish/register/$publishDate'
-      preLoaderRoute: typeof GNBLayoutPublishRegisterPublishDateIndexImport
-      parentRoute: typeof GNBLayoutImport
-    }
+      id: '/_GNBLayout/publish/register/$publishDate/';
+      path: '/publish/register/$publishDate';
+      fullPath: '/publish/register/$publishDate';
+      preLoaderRoute: typeof GNBLayoutPublishRegisterPublishDateIndexImport;
+      parentRoute: typeof GNBLayoutImport;
+    };
   }
 }
 
 // Create and export the route tree
 
 interface GNBLayoutRouteChildren {
-  GNBLayoutComponentIndexRoute: typeof GNBLayoutComponentIndexRoute
-  GNBLayoutProblemSetIndexRoute: typeof GNBLayoutProblemSetIndexRoute
-  GNBLayoutProblemIndexRoute: typeof GNBLayoutProblemIndexRoute
-  GNBLayoutPublishIndexRoute: typeof GNBLayoutPublishIndexRoute
-  GNBLayoutProblemSetProblemSetIdIndexRoute: typeof GNBLayoutProblemSetProblemSetIdIndexRoute
-  GNBLayoutProblemProblemIdIndexRoute: typeof GNBLayoutProblemProblemIdIndexRoute
-  GNBLayoutProblemRegisterIndexRoute: typeof GNBLayoutProblemRegisterIndexRoute
-  GNBLayoutPublishRegisterPublishDateIndexRoute: typeof GNBLayoutPublishRegisterPublishDateIndexRoute
+  GNBLayoutComponentIndexRoute: typeof GNBLayoutComponentIndexRoute;
+  GNBLayoutProblemSetIndexRoute: typeof GNBLayoutProblemSetIndexRoute;
+  GNBLayoutProblemIndexRoute: typeof GNBLayoutProblemIndexRoute;
+  GNBLayoutPublishIndexRoute: typeof GNBLayoutPublishIndexRoute;
+  GNBLayoutProblemSetProblemSetIdIndexRoute: typeof GNBLayoutProblemSetProblemSetIdIndexRoute;
+  GNBLayoutProblemProblemIdIndexRoute: typeof GNBLayoutProblemProblemIdIndexRoute;
+  GNBLayoutProblemRegisterIndexRoute: typeof GNBLayoutProblemRegisterIndexRoute;
+  GNBLayoutPublishRegisterPublishDateIndexRoute: typeof GNBLayoutPublishRegisterPublishDateIndexRoute;
 }
 
 const GNBLayoutRouteChildren: GNBLayoutRouteChildren = {
@@ -196,63 +195,59 @@ const GNBLayoutRouteChildren: GNBLayoutRouteChildren = {
   GNBLayoutProblemSetIndexRoute: GNBLayoutProblemSetIndexRoute,
   GNBLayoutProblemIndexRoute: GNBLayoutProblemIndexRoute,
   GNBLayoutPublishIndexRoute: GNBLayoutPublishIndexRoute,
-  GNBLayoutProblemSetProblemSetIdIndexRoute:
-    GNBLayoutProblemSetProblemSetIdIndexRoute,
+  GNBLayoutProblemSetProblemSetIdIndexRoute: GNBLayoutProblemSetProblemSetIdIndexRoute,
   GNBLayoutProblemProblemIdIndexRoute: GNBLayoutProblemProblemIdIndexRoute,
   GNBLayoutProblemRegisterIndexRoute: GNBLayoutProblemRegisterIndexRoute,
-  GNBLayoutPublishRegisterPublishDateIndexRoute:
-    GNBLayoutPublishRegisterPublishDateIndexRoute,
-}
+  GNBLayoutPublishRegisterPublishDateIndexRoute: GNBLayoutPublishRegisterPublishDateIndexRoute,
+};
 
-const GNBLayoutRouteWithChildren = GNBLayoutRoute._addFileChildren(
-  GNBLayoutRouteChildren,
-)
+const GNBLayoutRouteWithChildren = GNBLayoutRoute._addFileChildren(GNBLayoutRouteChildren);
 
 export interface FileRoutesByFullPath {
-  '/': typeof IndexRoute
-  '': typeof GNBLayoutRouteWithChildren
-  '/login': typeof LoginIndexRoute
-  '/component': typeof GNBLayoutComponentIndexRoute
-  '/problem-set': typeof GNBLayoutProblemSetIndexRoute
-  '/problem': typeof GNBLayoutProblemIndexRoute
-  '/publish': typeof GNBLayoutPublishIndexRoute
-  '/problem-set/$problemSetId': typeof GNBLayoutProblemSetProblemSetIdIndexRoute
-  '/problem/$problemId': typeof GNBLayoutProblemProblemIdIndexRoute
-  '/problem/register': typeof GNBLayoutProblemRegisterIndexRoute
-  '/publish/register/$publishDate': typeof GNBLayoutPublishRegisterPublishDateIndexRoute
+  '/': typeof IndexRoute;
+  '': typeof GNBLayoutRouteWithChildren;
+  '/login': typeof LoginIndexRoute;
+  '/component': typeof GNBLayoutComponentIndexRoute;
+  '/problem-set': typeof GNBLayoutProblemSetIndexRoute;
+  '/problem': typeof GNBLayoutProblemIndexRoute;
+  '/publish': typeof GNBLayoutPublishIndexRoute;
+  '/problem-set/$problemSetId': typeof GNBLayoutProblemSetProblemSetIdIndexRoute;
+  '/problem/$problemId': typeof GNBLayoutProblemProblemIdIndexRoute;
+  '/problem/register': typeof GNBLayoutProblemRegisterIndexRoute;
+  '/publish/register/$publishDate': typeof GNBLayoutPublishRegisterPublishDateIndexRoute;
 }
 
 export interface FileRoutesByTo {
-  '/': typeof IndexRoute
-  '': typeof GNBLayoutRouteWithChildren
-  '/login': typeof LoginIndexRoute
-  '/component': typeof GNBLayoutComponentIndexRoute
-  '/problem-set': typeof GNBLayoutProblemSetIndexRoute
-  '/problem': typeof GNBLayoutProblemIndexRoute
-  '/publish': typeof GNBLayoutPublishIndexRoute
-  '/problem-set/$problemSetId': typeof GNBLayoutProblemSetProblemSetIdIndexRoute
-  '/problem/$problemId': typeof GNBLayoutProblemProblemIdIndexRoute
-  '/problem/register': typeof GNBLayoutProblemRegisterIndexRoute
-  '/publish/register/$publishDate': typeof GNBLayoutPublishRegisterPublishDateIndexRoute
+  '/': typeof IndexRoute;
+  '': typeof GNBLayoutRouteWithChildren;
+  '/login': typeof LoginIndexRoute;
+  '/component': typeof GNBLayoutComponentIndexRoute;
+  '/problem-set': typeof GNBLayoutProblemSetIndexRoute;
+  '/problem': typeof GNBLayoutProblemIndexRoute;
+  '/publish': typeof GNBLayoutPublishIndexRoute;
+  '/problem-set/$problemSetId': typeof GNBLayoutProblemSetProblemSetIdIndexRoute;
+  '/problem/$problemId': typeof GNBLayoutProblemProblemIdIndexRoute;
+  '/problem/register': typeof GNBLayoutProblemRegisterIndexRoute;
+  '/publish/register/$publishDate': typeof GNBLayoutPublishRegisterPublishDateIndexRoute;
 }
 
 export interface FileRoutesById {
-  __root__: typeof rootRoute
-  '/': typeof IndexRoute
-  '/_GNBLayout': typeof GNBLayoutRouteWithChildren
-  '/login/': typeof LoginIndexRoute
-  '/_GNBLayout/component/': typeof GNBLayoutComponentIndexRoute
-  '/_GNBLayout/problem-set/': typeof GNBLayoutProblemSetIndexRoute
-  '/_GNBLayout/problem/': typeof GNBLayoutProblemIndexRoute
-  '/_GNBLayout/publish/': typeof GNBLayoutPublishIndexRoute
-  '/_GNBLayout/problem-set/$problemSetId/': typeof GNBLayoutProblemSetProblemSetIdIndexRoute
-  '/_GNBLayout/problem/$problemId/': typeof GNBLayoutProblemProblemIdIndexRoute
-  '/_GNBLayout/problem/register/': typeof GNBLayoutProblemRegisterIndexRoute
-  '/_GNBLayout/publish/register/$publishDate/': typeof GNBLayoutPublishRegisterPublishDateIndexRoute
+  __root__: typeof rootRoute;
+  '/': typeof IndexRoute;
+  '/_GNBLayout': typeof GNBLayoutRouteWithChildren;
+  '/login/': typeof LoginIndexRoute;
+  '/_GNBLayout/component/': typeof GNBLayoutComponentIndexRoute;
+  '/_GNBLayout/problem-set/': typeof GNBLayoutProblemSetIndexRoute;
+  '/_GNBLayout/problem/': typeof GNBLayoutProblemIndexRoute;
+  '/_GNBLayout/publish/': typeof GNBLayoutPublishIndexRoute;
+  '/_GNBLayout/problem-set/$problemSetId/': typeof GNBLayoutProblemSetProblemSetIdIndexRoute;
+  '/_GNBLayout/problem/$problemId/': typeof GNBLayoutProblemProblemIdIndexRoute;
+  '/_GNBLayout/problem/register/': typeof GNBLayoutProblemRegisterIndexRoute;
+  '/_GNBLayout/publish/register/$publishDate/': typeof GNBLayoutPublishRegisterPublishDateIndexRoute;
 }
 
 export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath
+  fileRoutesByFullPath: FileRoutesByFullPath;
   fullPaths:
     | '/'
     | ''
@@ -264,8 +259,8 @@ export interface FileRouteTypes {
     | '/problem-set/$problemSetId'
     | '/problem/$problemId'
     | '/problem/register'
-    | '/publish/register/$publishDate'
-  fileRoutesByTo: FileRoutesByTo
+    | '/publish/register/$publishDate';
+  fileRoutesByTo: FileRoutesByTo;
   to:
     | '/'
     | ''
@@ -277,7 +272,7 @@ export interface FileRouteTypes {
     | '/problem-set/$problemSetId'
     | '/problem/$problemId'
     | '/problem/register'
-    | '/publish/register/$publishDate'
+    | '/publish/register/$publishDate';
   id:
     | '__root__'
     | '/'
@@ -290,25 +285,25 @@ export interface FileRouteTypes {
     | '/_GNBLayout/problem-set/$problemSetId/'
     | '/_GNBLayout/problem/$problemId/'
     | '/_GNBLayout/problem/register/'
-    | '/_GNBLayout/publish/register/$publishDate/'
-  fileRoutesById: FileRoutesById
+    | '/_GNBLayout/publish/register/$publishDate/';
+  fileRoutesById: FileRoutesById;
 }
 
 export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute
-  GNBLayoutRoute: typeof GNBLayoutRouteWithChildren
-  LoginIndexRoute: typeof LoginIndexRoute
+  IndexRoute: typeof IndexRoute;
+  GNBLayoutRoute: typeof GNBLayoutRouteWithChildren;
+  LoginIndexRoute: typeof LoginIndexRoute;
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   GNBLayoutRoute: GNBLayoutRouteWithChildren,
   LoginIndexRoute: LoginIndexRoute,
-}
+};
 
 export const routeTree = rootRoute
   ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>()
+  ._addFileTypes<FileRouteTypes>();
 
 /* ROUTE_MANIFEST_START
 {
