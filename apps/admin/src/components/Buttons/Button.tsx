c57b5099f6805284b64f7784618c9d58d739c6b9
Yoo TaeSeung <remicon99@gmail.com>
2025-02-24 04:02:47 +0900

    design: Button 컴포넌트에 full 사이즈 추가

--- apps/admin/src/components/Buttons/Button.tsx
+++ apps/admin/src/components/Buttons/Button.tsx
@@ -3,7 +3,7 @@ import { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'blue' | 'dark' | 'light' | 'dimmed';
-  sizeType?: 'short' | 'long';
+  sizeType?: 'short' | 'long' | 'full';
   disabled?: boolean;
   children: React.ReactNode;
 }
@@ -21,6 +21,7 @@ const Button = ({
   const sizeStyles = {
     short: 'min-w-[12rem] w-[12rem] h-[5.6rem]',
     long: 'min-w-[34.8rem] w-[34.8rem] h-[5.6rem]',
+    full: 'w-full h-[5.6rem]',
   };
 
   const variantStyles = {
