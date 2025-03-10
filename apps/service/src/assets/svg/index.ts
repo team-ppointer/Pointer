b6fb7c6da1685b3d28eb36b1dd64cab40e790db5
Yoo TaeSeung <remicon99@gmail.com>
2025-03-10 21:13:22 +0900

    feat(service): svg파일 추가

--- apps/service/src/assets/svg/index.ts
+++ apps/service/src/assets/svg/index.ts
@@ -1,8 +1,12 @@
 export { default as IcCopy } from './IcCopy';
+export { default as IcCorrect } from './IcCorrect';
 export { default as IcDirect } from './IcDirect';
+export { default as IcHome } from './IcHome';
+export { default as IcIncorrect } from './IcIncorrect';
 export { default as IcNextBlack } from './IcNextBlack';
 export { default as IcNext } from './IcNext';
 export { default as IcPrevBlack } from './IcPrevBlack';
 export { default as IcPrev } from './IcPrev';
+export { default as IcSetting } from './IcSetting';
 export { default as IcSolve } from './IcSolve';
 export { default as IcStepByStep } from './IcStepByStep';
