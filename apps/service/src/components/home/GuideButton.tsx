3168856dcecc1688c777c6f39d8f0dca85ec06f3
Yoo TaeSeung <remicon99@gmail.com>
2025-03-13 11:58:58 +0900

    design(service): GuideButton 너비 수정

--- apps/service/src/components/home/GuideButton.tsx
+++ apps/service/src/components/home/GuideButton.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 const GuideButton = () => {
   return (
-    <Link href='https://naver.com' target='_blank'>
-      <div className='flex min-w-[12rem] flex-col items-start gap-[0.8rem] rounded-[16px] bg-white px-[2.4rem] py-[2rem]'>
+    <Link href='https://naver.com' target='_blank' className='w-full min-w-[12rem]'>
+      <div className='flex w-full flex-col items-start gap-[0.8rem] rounded-[16px] bg-white px-[2.4rem] py-[2rem]'>
         <IcGuide width={24} height={24} />
         <div className='flex flex-col items-start'>
           <span className='font-bold-16'>포인터</span>
