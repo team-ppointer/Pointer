203477be54b5cfb109bd20085eb4e22e06e4f9b1
Yoo TaeSeung <remicon99@gmail.com>
2025-03-13 13:32:28 +0900

    chore(service): 공지 기능 숨기기

--- apps/service/src/app/(home)/page.tsx
+++ apps/service/src/app/(home)/page.tsx
@@ -11,7 +11,7 @@ const Page = () => {
         <p className='font-medium-12 text-lightgray500 pt-[1.6rem]'>
           아직은 고등학교 2학년 대상으로만 서비스를 하고 있어요!
         </p>
-        {true && <NoticeButton count={1} />}
+        {false && <NoticeButton count={1} />}
         <div className='flex w-full items-center gap-[1.2rem] pt-[1.6rem]'>
           <GuideButton />
           <WeekProgress startDate='03/10' endDate='14' />
