f49c91a59c70559bab9b31476036ea171a169dae
Yoo TaeSeung <remicon99@gmail.com>
2025-03-19 00:35:09 +0900

    design(service): '문제를 읽어내려갈 때', '진단 및 처방' 페이지 간격 및 텍스트 수정

--- apps/service/src/app/report/[publishId]/[problemId]/prescription/detail/page.tsx
+++ apps/service/src/app/report/[publishId]/[problemId]/prescription/detail/page.tsx
@@ -24,18 +24,18 @@ const Page = () => {
 
   const title = `${type === 'child' ? '새끼' : '메인'} 문제 ${problemNumber}${
     type === 'child' ? `-${childNumber}` : ''
-  }번 진단 및 처방`;
+  }번`;
 
   return (
     <>
-      <Header title='' iconType='back' />
+      <Header title='진단 및 처방' iconType='back' />
       <main className='px-[2rem] py-[8rem]'>
         <h1 className='font-bold-18 text-main my-[0.8rem]'>{title}</h1>
-        <div className='mt-[1.6rem] flex flex-col gap-[1.2rem] md:flex-row'>
+        <div className='mt-[1.6rem] flex flex-col gap-[1.6rem] md:flex-row'>
           <div className='w-full'>
             <img src={problemImageUrl} alt='problem' className='w-full rounded-[1.6rem]' />
           </div>
-          <div className='flex w-full flex-col gap-[1.2rem]'>
+          <div className='flex w-full flex-col gap-[1.6rem]'>
             {(solutionImageUrls ?? []).map((url, index) => (
               <img key={index} src={url} alt='solution' className='w-full rounded-[1.6rem]' />
             ))}
