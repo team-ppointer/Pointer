f38d624e682fe876a4a127f12a4320dd977c7eef
Yoo TaeSeung <remicon99@gmail.com>
2025-03-19 00:13:47 +0900

    fix(service): Suspense Boundary 빌드 오류 해결

--- apps/service/src/app/@modal/(.)image-modal/page.tsx
+++ apps/service/src/app/@modal/(.)image-modal/page.tsx
@@ -1,23 +1,19 @@
 'use client';
-
 import { Modal } from '@components';
 import { useSearchParams } from 'next/navigation';
 
 const page = () => {
   const searchParams = useSearchParams();
   const imageUrl = searchParams.get('imageUrl');
-  console.log(imageUrl);
 
   return (
-    <>
-      <Modal>
-        <img
-          src={imageUrl ?? ''}
-          alt='full image'
-          className='max-h-[calc(100dvh-8rem)] w-[calc(100dvw-8rem)] max-w-[100rem] object-contain'
-        />
-      </Modal>
-    </>
+    <Modal>
+      <img
+        src={imageUrl ?? ''}
+        alt='full image'
+        className='max-h-[calc(100dvh-8rem)] w-[calc(100dvw-8rem)] max-w-[100rem] object-contain'
+      />
+    </Modal>
   );
 };
 
