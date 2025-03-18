d3606d462bba2e4e935ec7214cfc97b08a418ec1
Yoo TaeSeung <remicon99@gmail.com>
2025-03-18 23:17:48 +0900

    feat(service): Divider 컴포넌트 추가

--- apps/service/src/components/common/index.ts
+++ apps/service/src/components/common/index.ts
@@ -6,9 +6,20 @@ import StatusIcon from './StatusIcon';
 import Header from './Header';
 import ProgressBar from './ProgressBar';
 import NavigationFooter from './NavigationFooter';
+import Divider from './Divider';
 
 export * from './Buttons';
 export * from './Inputs';
 export * from './Modals';
 
-export { Tag, TimeTag, ProgressBox, StatusTag, StatusIcon, Header, ProgressBar, NavigationFooter };
+export {
+  Tag,
+  TimeTag,
+  ProgressBox,
+  StatusTag,
+  StatusIcon,
+  Header,
+  ProgressBar,
+  NavigationFooter,
+  Divider,
+};
