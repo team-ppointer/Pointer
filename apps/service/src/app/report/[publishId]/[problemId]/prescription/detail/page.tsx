94c79cd767198f4eeac0d4f626bc240f45d391a2
Yoo TaeSeung <remicon99@gmail.com>
2025-03-19 00:02:37 +0900

    feat(service): 진단 및 처방 상세 페이지 퍼블리싱

--- apps/service/src/app/report/[publishId]/[problemId]/prescription/detail/page.tsx
+++ apps/service/src/app/report/[publishId]/[problemId]/prescription/detail/page.tsx
@@ -1,5 +1,49 @@
+'use client';
+
+import { Header } from '@components';
+import { useSearchParams } from 'next/navigation';
+
+import { useReport } from '@/hooks/report';
+
 const Page = () => {
-  return <div>Page</div>;
+  const searchParams = useSearchParams();
+  const type = searchParams.get('type');
+  const childNumber = searchParams.get('childNumber');
+
+  const { problemNumber, prescription } = useReport();
+
+  const problemImageUrl =
+    type === 'child'
+      ? prescription.childProblem[Number(childNumber) - 1]?.problem
+      : prescription.mainProblem.problem;
+
+  const solutionImageUrls =
+    type === 'child'
+      ? prescription.childProblem[Number(childNumber) - 1]?.solution
+      : prescription.mainProblem.solution;
+
+  const title = `${type === 'child' ? '새끼' : '메인'} 문제 ${problemNumber}${
+    type === 'child' ? `-${childNumber}` : ''
+  }번 진단 및 처방`;
+
+  return (
+    <>
+      <Header title='' iconType='back' />
+      <main className='px-[2rem] py-[8rem]'>
+        <h1 className='font-bold-18 text-main my-[0.8rem]'>{title}</h1>
+        <div className='mt-[1.6rem] flex flex-col gap-[1.2rem] md:flex-row'>
+          <div className='w-full'>
+            <img src={problemImageUrl} alt='problem' className='w-full rounded-[1.6rem]' />
+          </div>
+          <div className='flex w-full flex-col gap-[1.2rem]'>
+            {(solutionImageUrls ?? []).map((url, index) => (
+              <img key={index} src={url} alt='solution' className='w-full rounded-[1.6rem]' />
+            ))}
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Page;
--- apps/service/src/app/report/[publishId]/[problemId]/prescription/page.tsx
+++ apps/service/src/app/report/[publishId]/[problemId]/prescription/page.tsx
@@ -3,10 +3,13 @@ import { Divider, NavigationFooter } from '@components';
 import { useParams, useRouter } from 'next/navigation';
 
 import { PrescriptionCard, ReportHeader } from '@/components/report';
+import { useReport } from '@/hooks/report';
 
 const Page = () => {
   const router = useRouter();
-  const { publishId } = useParams();
+  const { publishId, problemId } = useParams();
+
+  const { problemNumber, prescription } = useReport();
 
   return (
     <>
@@ -15,12 +18,29 @@ const Page = () => {
         <h1 className='font-bold-18 text-main my-[0.8rem]'>포인팅</h1>
 
         <ul className='flex flex-col gap-[1.6rem] pt-[1.2rem]'>
-          <PrescriptionCard status='진단 완료' title='새끼 문항 1번' onClick={() => {}} />
-          <PrescriptionCard status='진단 완료' title='새끼 문항 2번' onClick={() => {}} />
-          <PrescriptionCard status='진단 완료' title='새끼 문항 3번' onClick={() => {}} />
+          {prescription.childProblem.map((childProblem) => {
+            return (
+              <PrescriptionCard
+                key={childProblem.childProblemNumber}
+                status='진단 완료'
+                title={`새끼 문항 ${problemNumber}-${childProblem.childProblemNumber}번`}
+                onClick={() =>
+                  router.push(
+                    `/report/${publishId}/${problemId}/prescription/detail?type=child&childNumber=${childProblem.childProblemNumber}`
+                  )
+                }
+              />
+            );
+          })}
 
           <Divider />
-          <PrescriptionCard status='진단 완료' title='메인 문항 1번' onClick={() => {}} />
+          <PrescriptionCard
+            status='진단 완료'
+            title={`메인 문항 ${problemNumber}번`}
+            onClick={() =>
+              router.push(`/report/${publishId}/${problemId}/prescription/detail?type=main`)
+            }
+          />
         </ul>
 
         <NavigationFooter
