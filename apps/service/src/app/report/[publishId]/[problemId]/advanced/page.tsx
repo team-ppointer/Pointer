7132a2a522278d13d3edbea9747bbd5365e4fcdd
Yoo TaeSeung <remicon99@gmail.com>
2025-03-25 02:58:44 +0900

    feat(service): 한걸음더 페이지 API 연동

--- apps/service/src/app/report/[publishId]/[problemId]/advanced/page.tsx
+++ apps/service/src/app/report/[publishId]/[problemId]/advanced/page.tsx
@@ -8,7 +8,9 @@ const Page = () => {
   const { publishId, problemId } = useParams();
   const router = useRouter();
 
-  const { advanced } = useReport();
+  const { problemNumber, seniorTipImageUrl, prescription } = useReport();
+
+  const mainImageUrl = prescription?.mainProblem?.imageUrl;
 
   return (
     <>
@@ -19,17 +21,21 @@ const Page = () => {
           <SmallButton
             variant='underline'
             sizeType='small'
-            onClick={() => router.push(`/image-modal?imageUrl=${advanced}`)}>
-            메인 문제 1번 다시 보기
+            onClick={() => router.push(`/image-modal?imageUrl=${mainImageUrl}`)}>
+            메인 문제 {problemNumber}번 다시 보기
           </SmallButton>
         </div>
         <div className='mt-[2.4rem] flex flex-col gap-[1.6rem]'>
-          <img src={advanced} alt='advanced' className={`w-full rounded-[1.6rem] object-contain`} />
+          <img
+            src={seniorTipImageUrl}
+            alt='advanced'
+            className={`w-full rounded-[1.6rem] object-contain`}
+          />
         </div>
         <NavigationFooter
           prevLabel='해설'
           nextLabel='진단 및 분석'
-          onClickPrev={() => router.back()}
+          onClickPrev={() => router.push(`/report/${publishId}/${problemId}/analysis`)}
           onClickNext={() => router.push(`/report/${publishId}/${problemId}/prescription`)}
         />
       </main>
