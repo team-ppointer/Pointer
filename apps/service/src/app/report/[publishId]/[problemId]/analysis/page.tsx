c13d09b5452133ec847c129323986fb52dddc4b6
Yoo TaeSeung <remicon99@gmail.com>
2025-03-25 03:10:14 +0900

    feat(service): 분석 & 손해설 페이지 정답 표시 추가

--- apps/service/src/app/report/[publishId]/[problemId]/analysis/page.tsx
+++ apps/service/src/app/report/[publishId]/[problemId]/analysis/page.tsx
@@ -11,13 +11,20 @@ import { TabMenu } from '@/components/report';
 const Page = () => {
   const router = useRouter();
   const { publishId, problemId } = useParams();
-  const { problemNumber, mainAnalysisImageUrl, mainHandwritingExplanationImageUrl } = useReport();
+  const { problemNumber, answer, mainAnalysisImageUrl, mainHandwritingExplanationImageUrl } =
+    useReport();
   const [selectedTab, setSelectedTab] = useState<'분석' | '손해설'>('분석');
   return (
     <>
       <ProgressHeader progress={33} />
       <main className='px-[2rem] py-[8rem]'>
-        <h1 className='font-bold-18 text-main my-[0.8rem]'>메인 문제 {problemNumber}번</h1>
+        <header className='flex items-center justify-between'>
+          <h1 className='font-bold-18 text-main my-[0.8rem]'>메인 문제 {problemNumber}번</h1>
+          <div className='flex items-center gap-[0.8rem]'>
+            <span className='font-medium-16 text-black'>정답</span>
+            <span className='font-medium-16 text-main'>{answer}번</span>
+          </div>
+        </header>
         <div className='mt-[2.4rem] flex flex-col gap-[1.6rem]'>
           <TabMenu
             leftMenu='분석'
