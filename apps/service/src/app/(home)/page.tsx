02d37842e362ff620aece8eb36b67d5bf7d75eb8
Yoo TaeSeung <remicon99@gmail.com>
2025-03-27 03:54:50 +0900

    fix(service): 마이페이지 메뉴 수정

--- apps/service/src/app/(home)/page.tsx
+++ apps/service/src/app/(home)/page.tsx
@@ -26,7 +26,7 @@ const Page = async () => {
 
   return (
     <>
-      <HomeHeader grade={2} name='홍길동' />
+      <HomeHeader name='홍길동' grade={2} />
       <main className='flex flex-col px-[2rem] pt-[6rem]'>
         <p className='font-medium-12 text-lightgray500 pt-[1.6rem]'>
           아직은 고등학교 2학년 대상으로만 서비스를 하고 있어요!
--- apps/service/src/components/my-page/NameCard.tsx
+++ apps/service/src/components/my-page/NameCard.tsx
@@ -5,14 +5,14 @@ interface NameCardProps {
   grade: number;
 }
 
-const NameCard = ({ name, grade }: NameCardProps) => {
+const NameCard = ({ name }: NameCardProps) => {
   return (
     <article className='center my-[2rem] flex gap-[0.8rem] rounded-[16px] bg-white p-[2rem]'>
       <p className='font-bold-18 text-black'>
         <strong className='text-main mr-[0.4rem]'>{name}</strong>
         <span>님</span>
       </p>
-      <p className='font-medium-14 text-lightgray500 flex items-end'>고등학교 {grade}학년</p>
+      {/* <p className='font-medium-14 text-lightgray500 flex items-end'>고등학교 {grade}학년</p> */}
     </article>
   );
 };
