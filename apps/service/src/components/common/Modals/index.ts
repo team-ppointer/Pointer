2b9bad49e73709a5176c415f30a37b98c669451a
Yoo TaeSeung <remicon99@gmail.com>
2025-03-27 02:47:53 +0900

    feat(service): 채점 모달 추가

--- apps/service/src/components/common/Modals/index.ts
+++ apps/service/src/components/common/Modals/index.ts
@@ -1,7 +1,13 @@
 import PortalModal from './PortalModal';
 import RouteModal from './RouteModal';
 import BaseModalTemplate from './templates/BaseModalTemplate';
-import CorrectModalTemplate from './templates/CorrectModalTemplate';
+import AnswerCheckModalTemplate from './templates/AnswerCheckModalTemplate';
 import TwoButtonModalTemplate from './templates/TwoButtonModalTemplate';
 
-export { PortalModal, RouteModal, BaseModalTemplate, CorrectModalTemplate, TwoButtonModalTemplate };
+export {
+  PortalModal,
+  RouteModal,
+  BaseModalTemplate,
+  AnswerCheckModalTemplate,
+  TwoButtonModalTemplate,
+};
--- apps/service/src/components/problem/AnswerInputForm.tsx
+++ apps/service/src/components/problem/AnswerInputForm.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { putProblemSubmit } from '@apis';
-import { AnswerInput, Button } from '@components';
-import { ProblemAnswerType } from '@types';
+import { AnswerInput, Button, AnswerCheckModalTemplate, PortalModal } from '@components';
+import { useModal } from '@hooks';
+import { ProblemAnswerType, ProblemStatus } from '@types';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 interface AnswerInputFormProps {
@@ -21,29 +23,40 @@ const AnswerInputForm = ({
   answerType = 'MULTIPLE_CHOICE',
   childProblemId,
 }: AnswerInputFormProps) => {
+  const { isOpen, openModal, closeModal } = useModal();
+  const [result, setResult] = useState<ProblemStatus | undefined>();
   const { register, handleSubmit, watch } = useForm<AnswerType>();
   const selectedAnswer = watch('answer');
 
-  const onSubmitAnswer: SubmitHandler<AnswerType> = (data) => {
+  const onSubmitAnswer: SubmitHandler<AnswerType> = async (data) => {
     if (childProblemId) {
       console.log(data);
     } else {
-      putProblemSubmit(publishId, problemId, data.answer);
+      const { data: resultData } = await putProblemSubmit(publishId, problemId, data.answer);
+      setResult(resultData?.data);
+      if (resultData) {
+        openModal();
+      }
     }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmitAnswer)}>
-      <h3 className='font-bold-16 text-black'>정답 선택</h3>
-      <div className='mt-[1.2rem] flex flex-col gap-[2rem] lg:flex-row'>
-        <AnswerInput
-          answerType={answerType}
-          selectedAnswer={selectedAnswer}
-          {...register('answer')}
-        />
-        <Button>제출하기</Button>
-      </div>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmitAnswer)}>
+        <h3 className='font-bold-16 text-black'>정답 선택</h3>
+        <div className='mt-[1.2rem] flex flex-col gap-[2rem] lg:flex-row'>
+          <AnswerInput
+            answerType={answerType}
+            selectedAnswer={selectedAnswer}
+            {...register('answer')}
+          />
+          <Button>제출하기</Button>
+        </div>
+      </form>
+      <PortalModal isOpen={isOpen} onClose={closeModal}>
+        <AnswerCheckModalTemplate result={result} onClose={closeModal} />
+      </PortalModal>
+    </>
   );
 };
 
