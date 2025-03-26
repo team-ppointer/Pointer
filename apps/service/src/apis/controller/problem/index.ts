cf1eaab8705514d49ecec52170f318a3e9b460b2
Yoo TaeSeung <remicon99@gmail.com>
2025-03-26 16:51:41 +0900

    feat(service): 문제 썸네일 페이지 API 연동

--- apps/service/src/apis/controller/problem/index.ts
+++ apps/service/src/apis/controller/problem/index.ts
@@ -1,5 +1,6 @@
 import getProblemAll from './getProblemAll';
 import getProblemById from './getProblemById';
 import getProblemsByPublishId from './getProblemsByPublishId';
+import getProblemThumbnail from './getProblemThumbnail';
 
-export { getProblemAll, getProblemById, getProblemsByPublishId };
+export { getProblemAll, getProblemById, getProblemsByPublishId, getProblemThumbnail };
--- apps/service/src/app/problem/solve/[publishId]/[problemId]/page.tsx
+++ apps/service/src/app/problem/solve/[publishId]/[problemId]/page.tsx
@@ -1,17 +1,25 @@
+import { getProblemThumbnail } from '@apis';
 import { ProgressHeader, SolveButton, TimeTag } from '@components';
 
-const Page = () => {
+const Page = async ({ params }: { params: Promise<{ publishId: string; problemId: string }> }) => {
+  const { publishId, problemId } = await params;
+
+  const { number, imageUrl, recommendedMinute, recommendedSecond } = await getProblemThumbnail(
+    publishId,
+    problemId
+  );
+
   return (
     <>
       <ProgressHeader progress={10} />
       <main className='p-[2rem] pt-[8rem]'>
         <div className='flex items-center justify-between'>
-          <h1 className='font-bold-18 text-main'>문제 1번</h1>
-          <TimeTag minutes={1} seconds={30} />
+          <h1 className='font-bold-18 text-main'>메인 문제 {number}번</h1>
+          <TimeTag minutes={recommendedMinute} seconds={recommendedSecond} />
         </div>
         <img
-          src='https://placehold.co/600x400'
-          alt='문제 1'
+          src={imageUrl}
+          alt={`메인 문제 ${number}번`}
           className='mt-[1.2rem] w-full object-contain'
         />
 
