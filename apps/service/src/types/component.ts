7b5e942746e520f8caaec3f8c26a7e64f020b715
Yoo TaeSeung <remicon99@gmail.com>
2025-03-27 02:47:40 +0900

    feat(service): 문제 상태 type 추가

--- apps/service/src/types/component.ts
+++ apps/service/src/types/component.ts
@@ -1,1 +1,10 @@
 export type ProblemAnswerType = 'MULTIPLE_CHOICE' | 'SHORT_ANSWER';
+
+export type ProblemStatus =
+  | 'CORRECT'
+  | 'INCORRECT'
+  | 'RETRY_CORRECT'
+  | 'IN_PROGRESS'
+  | 'NOT_STARTED';
+
+export type ChildProblemStatus = 'CORRECT' | 'INCORRECT' | 'RETRY_CORRECT' | 'NOT_STARTED';
