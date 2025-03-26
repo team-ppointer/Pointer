709c39990324ecf90d0e6c59332e948f1cadecb5
Yoo TaeSeung <remicon99@gmail.com>
2025-03-27 03:05:40 +0900

    refactor(service): useParams 타입 제네릭 추가

--- apps/service/src/contexts/ReportContext.tsx
+++ apps/service/src/contexts/ReportContext.tsx
@@ -10,10 +10,10 @@ type CommentaryGetResponse = components['schemas']['CommentaryGetResponse'];
 export const ReportContext = createContext<CommentaryGetResponse | null>(null);
 
 export const ReportProvider = ({ children }: { children: React.ReactNode }) => {
-  const { publishId, problemId } = useParams();
+  const { publishId, problemId } = useParams<{ publishId: string; problemId: string }>();
   const { data: reportData } = getCommentary({
-    publishId: publishId as string,
-    problemId: problemId as string,
+    publishId,
+    problemId,
   });
 
   return <ReportContext.Provider value={reportData?.data ?? {}}>{children}</ReportContext.Provider>;
