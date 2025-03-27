dfbe7451aa5ee2c3b26602272311bc059617014b
Yoo TaeSeung <remicon99@gmail.com>
2025-03-27 19:14:22 +0900

    feat(service): 새끼문항 풀이 페이지 API 연동

--- apps/service/src/app/problem/solve/[publishId]/[problemId]/child-problem/[childProblemId]/page.tsx
+++ apps/service/src/app/problem/solve/[publishId]/[problemId]/child-problem/[childProblemId]/page.tsx
@@ -1,32 +1,54 @@
 'use client';
-
 import { NavigationFooter, ProgressHeader, SmallButton } from '@components';
+import { useParams, useRouter } from 'next/navigation';
+import { getChildProblemById } from '@apis';
 
 import { AnswerInputForm } from '@/components/problem';
+import { useChildProblemContext } from '@/hooks/problem';
 
-type Params = {
-  publishId: string;
-  problemId: string;
-  childProblemId: string;
-};
+const Page = () => {
+  const { publishId, problemId, childProblemId } = useParams<{
+    publishId: string;
+    problemId: string;
+    childProblemId: string;
+  }>();
+  const router = useRouter();
+  const { childProblemLength, mainProblemImageUrl, onPrev, onNext } = useChildProblemContext();
 
-const Page = async ({ params }: { params: Promise<Params> }) => {
-  const { publishId, problemId, childProblemId } = await params;
+  const { data } = getChildProblemById(publishId, problemId, childProblemId);
+  const { problemNumber, childProblemNumber = 1, imageUrl, status } = data?.data ?? {};
+
+  const prevButtonLabel =
+    childProblemNumber === 1
+      ? `메인 문제 ${problemNumber}번`
+      : `새끼 문제 ${problemNumber}-${childProblemNumber - 1}번`;
+
+  const nextButtonLabel =
+    childProblemNumber === childProblemLength
+      ? `메인 문제 ${problemNumber}번`
+      : `새끼 문제 ${problemNumber}-${childProblemNumber + 1}번`;
+
+  const isSolved = status === 'CORRECT' || status === 'RETRY_CORRECT';
 
   return (
     <>
       <ProgressHeader progress={10} />
       <main className='flex flex-col px-[2rem] py-[8rem] md:flex-row md:gap-[4rem]'>
         <div className='w-full'>
-          <h1 className='font-bold-18 text-main'>문제 1번</h1>
+          <h1 className='font-bold-18 text-main'>
+            새끼 문제 {problemNumber}-{childProblemNumber}번
+          </h1>
           <img
-            src='https://placehold.co/600x400'
-            alt='문제 1'
+            src={imageUrl}
+            alt={`새끼 문제 ${problemNumber}-${childProblemNumber}번`}
             className='mt-[1.2rem] w-full object-contain'
           />
 
           <div className='mt-[0.6rem] mb-[0.4rem] flex items-center justify-end'>
-            <SmallButton variant='underline' sizeType='small'>
+            <SmallButton
+              variant='underline'
+              sizeType='small'
+              onClick={() => router.push(`/image-modal?imageUrl=${mainProblemImageUrl}`)}>
               메인 문제 다시보기
             </SmallButton>
           </div>
@@ -38,15 +60,16 @@ const Page = async ({ params }: { params: Promise<Params> }) => {
             problemId={problemId}
             childProblemId={childProblemId}
             answerType={'SHORT_ANSWER'}
+            isSolved={isSolved}
           />
         </div>
       </main>
 
       <NavigationFooter
-        prevLabel='이전'
-        nextLabel='다음'
-        onClickPrev={() => {}}
-        onClickNext={() => {}}
+        prevLabel={prevButtonLabel}
+        nextLabel={nextButtonLabel}
+        onClickPrev={onPrev}
+        onClickNext={onNext}
       />
     </>
   );
