92411164bdfbe588603f7a8394392bad7bc61ba2
Yoo TaeSeung <remicon99@gmail.com>
2025-03-27 23:28:00 +0900

    feat(service): 문제리스트 화면 해설 버튼 추가

--- apps/service/src/components/problem/ProblemStatusCard.tsx
+++ apps/service/src/components/problem/ProblemStatusCard.tsx
@@ -3,7 +3,7 @@
 import { Button, StatusIcon, StatusTag } from '@components';
 import { components } from '@schema';
 import { IcDown, IcUp } from '@svg';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
 type ProblemFeedProgressesGetResponse = components['schemas']['ProblemFeedProgressesGetResponse'];
@@ -19,15 +19,20 @@ const ProblemStatusCard = ({
   publishId,
   problemData,
 }: ProblemStatusCardProps) => {
+  const { problemId, status, childProblemStatuses } = problemData;
+  const router = useRouter();
   const [isOpen, setIsOpen] = useState(
-    !problemData.childProblemStatuses?.every((childStatus) => childStatus === 'NOT_STARTED')
+    !childProblemStatuses?.every((childStatus) => childStatus === 'NOT_STARTED')
   );
+
+  const isSolved = status === 'CORRECT' || status === 'RETRY_CORRECT' || status === 'INCORRECT';
+
   return (
     <article className='rounded-[16px] bg-white p-[2rem]'>
       <header className='flex items-center justify-between'>
         <div className='flex items-center justify-between'>
           <h2 className='font-bold-16 text-main w-[10rem]'>메인 문제 {mainProblemNumber}번</h2>
-          <StatusTag status={problemData.status ?? 'NOT_STARTED'} />
+          <StatusTag status={status ?? 'NOT_STARTED'} />
         </div>
         {isOpen ? (
           <IcUp width={24} height={24} onClick={() => setIsOpen((prev) => !prev)} />
@@ -38,7 +43,7 @@ const ProblemStatusCard = ({
 
       {isOpen && (
         <ul className='mt-[1.2rem] flex flex-col'>
-          {problemData.childProblemStatuses?.map((childStatus, index) => (
+          {childProblemStatuses?.map((childStatus, index) => (
             <li className='flex items-center justify-between py-[1.15rem]' key={index}>
               <p className='font-medium-14 text-black'>
                 새끼 문제 {mainProblemNumber}-{index + 1}번
@@ -49,9 +54,18 @@ const ProblemStatusCard = ({
         </ul>
       )}
 
-      <Link href={`/problem/solve/${publishId}/${problemData.problemId}`}>
-        <Button className='mt-[1.6rem]'>문제 풀러 가기</Button>
-      </Link>
+      <div className='mt-[1.6rem] flex gap-[0.8rem]'>
+        <Button
+          variant={isSolved ? 'light' : 'blue'}
+          onClick={() => router.push(`/problem/solve/${publishId}/${problemId}`)}>
+          문제 풀러 가기
+        </Button>
+        {isSolved && (
+          <Button onClick={() => router.push(`/report/${publishId}/${problemId}/analysis`)}>
+            해설 보기
+          </Button>
+        )}
+      </div>
     </article>
   );
 };
