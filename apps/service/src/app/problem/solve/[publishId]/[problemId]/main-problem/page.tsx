5c559fdf9191a081edcd46402231a084b66cfb97
Yoo TaeSeung <remicon99@gmail.com>
2025-03-30 17:24:06 +0900

    feat(service): 새끼/메인 문제 정답/오답 태그 추가

--- apps/service/src/app/problem/solve/[publishId]/[problemId]/main-problem/page.tsx
+++ apps/service/src/app/problem/solve/[publishId]/[problemId]/main-problem/page.tsx
@@ -115,7 +115,16 @@ const Page = () => {
         <div className='w-full'>
           <div className='flex items-center justify-between'>
             <h1 className='font-bold-18 text-main'>메인 문제 {number}번</h1>
-            <TimeTag minutes={recommendedMinute} seconds={recommendedSecond} />
+            {isSolved && (
+              <Tag variant='green' sizeType='small'>
+                정답
+              </Tag>
+            )}
+            {status === 'INCORRECT' && (
+              <Tag variant='red' sizeType='small'>
+                오답
+              </Tag>
+            )}
           </div>
           <img
             src={imageUrl}
