d0590244e0f47475dc60abef05a74f2f8807990e
Yoo TaeSeung <remicon99@gmail.com>
2025-03-31 00:36:33 +0900

    feat(service): 카카오 로그인 연동

--- apps/service/src/apis/controller/auth/postKakaoLogin.ts
+++ apps/service/src/apis/controller/auth/postKakaoLogin.ts
@@ -1,6 +1,24 @@
-import { client } from '@/apis/client';
+'use client';
 
-const postKakaoLogin = async (accessToken: string) => {
+import { redirect } from 'next/navigation';
+
+import { client } from '@/apis/client';
+import { setAccessToken, setName } from '@/contexts/AuthContext';
+
+const postKakaoAccessToken = async (code: string) => {
+  const response = await fetch(`https://kauth.kakao.com/oauth/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+    },
+    body: `grant_type=authorization_code&client_id=${process.env.NEXT_PUBLIC_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&code=${code}`,
+  });
+  const jsonData = await response.json();
+  return jsonData.access_token;
+};
+
+const postKakaoLogin = async (code: string) => {
+  const accessToken = await postKakaoAccessToken(code);
   const response = await client.POST('/api/v1/auth/oauth/social-login', {
     params: {
       header: {
@@ -11,6 +29,27 @@ const postKakaoLogin = async (accessToken: string) => {
       },
     },
   });
+
+  try {
+    if (
+      response &&
+      response.data &&
+      response.data.data &&
+      // response.data.data.name &&
+      response.data.data.accessToken
+    ) {
+      const { accessToken, name } = response.data.data;
+      setAccessToken(accessToken);
+      setName(name || '');
+
+      window.location.href = '/';
+    } else {
+      console.error('accessToken을 찾을 수 없습니다:', response);
+    }
+  } catch (error) {
+    console.error('소셜 로그인 요청 오류:', error);
+  }
+
   return response;
 };
 
--- apps/service/src/app/api/auth/callback/kakao/page.tsx
+++ apps/service/src/app/api/auth/callback/kakao/page.tsx
@@ -1,37 +1,20 @@
-import { postKakaoAccessToken, postKakaoLogin } from '@apis';
-import { redirect } from 'next/navigation';
+'use client';
 
-import { setAccessToken } from '@/contexts/AuthContext';
+import { postKakaoLogin } from '@apis';
+import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
-const Page = async ({ searchParams }: { searchParams: Promise<{ code: string }> }) => {
-  const { code } = await searchParams;
+const Page = () => {
+  const searchParams = useSearchParams();
+  const code = searchParams.get('code');
 
-  try {
+  useEffect(() => {
     if (code) {
-      const kakaoAccessToken = await postKakaoAccessToken(code);
-      console.log('kakaoAccessToken', kakaoAccessToken);
-
-      if (kakaoAccessToken) {
-        try {
-          const response = await postKakaoLogin(kakaoAccessToken);
-          console.log('응답 데이터:', response);
-
-          if (response && response.data && response.data.data && response.data.data.accessToken) {
-            setAccessToken(response.data.data.accessToken);
-            redirect('/');
-          } else {
-            console.error('accessToken을 찾을 수 없습니다:', response);
-          }
-        } catch (error) {
-          console.error('소셜 로그인 요청 오류:', error);
-        }
-      }
+      postKakaoLogin(code);
     }
-  } catch (error) {
-    console.error('카카오 토큰 요청 오류:', error);
-  }
+  }, []);
 
-  return <div>code: {code}</div>;
+  return <></>;
 };
 
 export default Page;
