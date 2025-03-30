5f8232751d81335a977d3cd46f67d8070fa13d70
Yoo TaeSeung <remicon99@gmail.com>
2025-03-31 04:35:50 +0900

    fix(service): NameCard 폰트 수정

--- apps/service/src/components/my-page/NameCard.tsx
+++ apps/service/src/components/my-page/NameCard.tsx
@@ -13,7 +13,7 @@ const NameCard = () => {
   return (
     <article className='center my-[2rem] flex gap-[0.8rem] rounded-[16px] bg-white p-[2rem]'>
       <p className='font-bold-18 text-black'>
-        <strong className='text-main mr-[0.4rem]'>{name}</strong>
+        <span className='text-main mr-[0.4rem]'>{name}</span>
         <span>님</span>
       </p>
       {/* <p className='font-medium-14 text-lightgray500 flex items-end'>고등학교 {grade}학년</p> */}
