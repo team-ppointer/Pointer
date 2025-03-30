9f80e1931c498b11f94b52e854a9a01cdabde914
Yoo TaeSeung <remicon99@gmail.com>
2025-03-31 05:43:19 +0900

    fix(service): 문제를 읽어 내려갈때 버튼 위치 수정

--- apps/service/src/app/report/[publishId]/[problemId]/analysis/page.tsx
+++ apps/service/src/app/report/[publishId]/[problemId]/analysis/page.tsx
@@ -48,45 +48,48 @@ const Page = () => {
   return (
     <>
       <ProgressHeader progress={33} />
-      <main className='px-[2rem] py-[8rem]'>
-        <header className='flex items-center justify-between'>
-          <h1 className='font-bold-18 text-main my-[0.8rem]'>메인 문제 {problemNumber}번</h1>
-          <div className='flex items-center gap-[0.8rem]'>
-            <span className='font-medium-16 text-black'>정답</span>
-            <span className='font-medium-16 text-main'>
-              {answer}
-              {answerType === 'MULTIPLE_CHOICE' && '번'}
-            </span>
+      <main className='flex min-h-[100dvh] flex-col justify-between px-[2rem] py-[8rem]'>
+        <div>
+          <header className='flex items-center justify-between'>
+            <h1 className='font-bold-18 text-main my-[0.8rem]'>메인 문제 {problemNumber}번</h1>
+            <div className='flex items-center gap-[0.8rem]'>
+              <span className='font-medium-16 text-black'>정답</span>
+              <span className='font-medium-16 text-main'>
+                {answer}
+                {answerType === 'MULTIPLE_CHOICE' && '번'}
+              </span>
+            </div>
+          </header>
+          <div className='mt-[2.4rem] flex flex-col gap-[1.6rem]'>
+            <TabMenu
+              leftMenu='분석'
+              rightMenu='손해설'
+              selectedTab={selectedTab}
+              onTabChange={handleClickTab}
+            />
+            <ImageContainer className={`${selectedTab === '분석' ? 'block' : 'hidden'}`}>
+              <Image
+                src={mainAnalysisImageUrl ?? ''}
+                alt='analysis'
+                className={`w-full object-contain`}
+                width={700}
+                height={200}
+                priority
+              />
+            </ImageContainer>
+            <ImageContainer className={`${selectedTab === '손해설' ? 'block' : 'hidden'}`}>
+              <Image
+                src={mainHandwritingExplanationImageUrl ?? ''}
+                alt='handWriting'
+                className={`w-full object-contain`}
+                width={700}
+                height={200}
+                priority
+              />
+            </ImageContainer>
           </div>
-        </header>
-        <div className='mt-[2.4rem] flex flex-col gap-[1.6rem]'>
-          <TabMenu
-            leftMenu='분석'
-            rightMenu='손해설'
-            selectedTab={selectedTab}
-            onTabChange={handleClickTab}
-          />
-          <ImageContainer className={`${selectedTab === '분석' ? 'block' : 'hidden'}`}>
-            <Image
-              src={mainAnalysisImageUrl ?? ''}
-              alt='analysis'
-              className={`w-full object-contain`}
-              width={700}
-              height={200}
-              priority
-            />
-          </ImageContainer>
-          <ImageContainer className={`${selectedTab === '손해설' ? 'block' : 'hidden'}`}>
-            <Image
-              src={mainHandwritingExplanationImageUrl ?? ''}
-              alt='handWriting'
-              className={`w-full object-contain`}
-              width={700}
-              height={200}
-              priority
-            />
-          </ImageContainer>
-
+        </div>
+        <div>
           <button
             type='button'
             className='border-sub1 flex w-full items-center justify-between rounded-[1.6rem] border bg-white px-[2rem] py-[1.6rem]'
