887a4c222ce476f5e4fc846e38ee517bee29d7a7
Yoo TaeSeung <remicon99@gmail.com>
2025-03-31 05:34:46 +0900

    fix(service): 로그인 페이지 디자인 수정

--- apps/service/src/app/login/page.tsx
+++ apps/service/src/app/login/page.tsx
@@ -27,8 +27,8 @@ const Page = () => {
   }, [router]);
 
   return (
-    <div className='mx-auto flex h-[100dvh] w-[33.5rem] flex-col pb-[2rem]'>
-      <div className='flex flex-col items-center py-[9.6rem]'>
+    <div className='mx-auto flex h-[100dvh] w-[33.5rem] flex-col justify-center gap-[6.4rem]'>
+      <div className='mt-[6.4rem] flex flex-col items-center'>
         <p className='font-medium-16 text-center text-black'>
           손해설부터 처방까지
           <br />
@@ -37,7 +37,7 @@ const Page = () => {
         <LogoLogin width={250} height={57} className='mt-[2.4rem]' />
         <h1 className='text-main font-bold-24 mt-[1.6rem]'>포인터</h1>
       </div>
-      <div className='mt-auto flex flex-col items-center gap-[1.6rem]'>
+      <div className='flex flex-col items-center gap-[1.6rem]'>
         <p className='font-medium-12 text-lightgray500'>포인터는 태블릿의 스플릿뷰를 권장해요</p>
         <KakaoButton onClick={handleLoginClick} />
         {/* <AppleButton /> */}
