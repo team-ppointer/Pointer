94da2831722ceafeab9af7aab5f141a39ecb54b5
Yoo TaeSeung <remicon99@gmail.com>
2025-04-12 18:00:24 +0900

    feat(admin): Adds bundle analyzer for admin app

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -3,11 +3,22 @@ import react from '@vitejs/plugin-react-swc';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          filename: 'dist/stats.html',
+          open: false,
+          gzipSize: true,
+          brotliSize: true,
+        }),
+      ],
+    },
   },
   plugins: [TanStackRouterVite({}), react(), svgr(), tsconfigPaths()],
 });
