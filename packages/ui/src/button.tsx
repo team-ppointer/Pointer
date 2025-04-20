467fb921863f4a858cd362ba2b368a9b8664f70d
Yoo TaeSeung <remicon99@gmail.com>
2025-04-20 22:08:00 +0900

    feat(common): 공통 button 컴포넌트 생성

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,17 +1,36 @@
-'use client';
+import React from 'react';
+import { ButtonHTMLAttributes } from 'react';
 
-import { ReactNode } from 'react';
-
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'blue' | 'light';
+  disabled?: boolean;
+  children: React.ReactNode;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+const Button = ({
+  variant = 'blue',
+  disabled = false,
+  children,
+  className,
+  ...props
+}: ButtonProps) => {
+  const baseStyles =
+    'w-full h-[5.6rem] min-w-fit rounded-[16px] font-medium-16 flex items-center justify-center gap-[1.6rem] px-[1rem]';
+
+  const variantStyles = {
+    blue: 'bg-main text-white',
+    light: 'bg-white text-main border border-main',
+    disabled: 'bg-lightgray300 text-lightgray500',
+  };
+
   return (
-    <button className={className} onClick={() => alert(`Hello from your ${appName} app!`)}>
+    <button
+      className={`${baseStyles} ${variantStyles[disabled ? 'disabled' : variant]} ${className} `}
+      disabled={disabled}
+      {...props}>
       {children}
     </button>
   );
 };
+
+export default Button;
