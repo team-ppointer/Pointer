70a15de3278faee2b3a69658564e9794c2ad1472
Yoo TaeSeung <remicon99@gmail.com>
2025-04-20 23:45:52 +0900

    feat(common): Divider 공통 컴포넌트 생성

--- packages/pointer-design-system/src/components/index.ts
+++ packages/pointer-design-system/src/components/index.ts
@@ -1,4 +1,5 @@
 import Button from './Button';
 import PortalModal from './PortalModal';
+import Divider from './Divider';
 
-export { Button, PortalModal };
+export { Button, PortalModal, Divider };
