08fd89c23830b402e318318d75a0ca30cac446c2
Yoo TaeSeung <remicon99@gmail.com>
2025-07-02 17:34:11 +0900

    feat: 개념 태그 페이지 헤더 퍼블리싱

--- apps/admin/src/routes/_GNBLayout/concept-tags/index.tsx
+++ apps/admin/src/routes/_GNBLayout/concept-tags/index.tsx
@@ -1,9 +1,42 @@
+import { Button, Header, Input } from '@components';
 import { createFileRoute } from '@tanstack/react-router';
+import { useForm } from 'react-hook-form';
+import { Divider } from '@repo/pointer-design-system/components';
 
 export const Route = createFileRoute('/_GNBLayout/concept-tags/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/_GNBLayout/concept-tags/"!</div>;
+  const { register, handleSubmit, reset } = useForm<{
+    query: string;
+  }>();
+
+  const handleClickSearch = (data: { query: string }) => {
+    // Handle search logic here
+  };
+
+  return (
+    <>
+      <Header title='세트 목록' />
+      <form
+        className='my-[4.8rem] flex items-end justify-between gap-[3.2rem]'
+        onSubmit={handleSubmit(handleClickSearch)}>
+        <Input placeholder='검색어를 입력해주세요' {...register('query', { required: false })} />
+        <Button variant='dark'>검색</Button>
+      </form>
+      <Divider />
+      <div className='my-[4.8rem] flex items-center justify-between'>
+        <h2 className='font-bold-32 text-black'>개념 태그 리스트</h2>
+        <div className='flex gap-[1.6rem]'>
+          <Button sizeType='fit' variant='light'>
+            전체 선택 해제
+          </Button>
+          <Button sizeType='fit' variant='dark'>
+            선택 태그 삭제
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
