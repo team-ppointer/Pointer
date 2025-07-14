ab44970a9b9372c33326450b9723707b432414d2
chaeyoung103 <thdcodud103@naver.com>
2025-07-14 10:17:03 +0900

    fix(service): 작업진행을 위한 authmiddleware 로직 무력화

--- apps/service/src/apis/authMiddleware.ts
+++ apps/service/src/apis/authMiddleware.ts
@@ -3,69 +3,69 @@ import { Middleware } from 'openapi-fetch';
 
 import { getAccessToken, setAccessToken } from '@utils';
 
-const UNPROTECTED_ROUTES = ['/api/v1/auth/admin/login', '/api/student/auth/social/login'];
+// const UNPROTECTED_ROUTES = ['/api/v1/auth/admin/login', '/api/student/auth/social/login'];
 
-const reissueToken = async () => {
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/common/auth/refresh`,
-      {
-        method: 'GET',
-        credentials: 'include',
-      }
-    );
+// const reissueToken = async () => {
+//   try {
+//     const response = await fetch(
+//       `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/common/auth/refresh`,
+//       {
+//         method: 'GET',
+//         credentials: 'include',
+//       }
+//     );
 
-    if (!response.ok) throw new Error('Token reissue failed');
+//     if (!response.ok) throw new Error('Token reissue failed');
 
-    const data = await response.json();
-    const accessToken = data.data.accessToken;
-    setAccessToken(accessToken);
-    return accessToken;
-  } catch (error) {
-    console.error('Reissue failed:', error);
-    localStorage.removeItem('accessToken');
-    window.location.href = '/login';
-    return null;
-  }
-};
+//     const data = await response.json();
+//     const accessToken = data.data.accessToken;
+//     setAccessToken(accessToken);
+//     return accessToken;
+//   } catch (error) {
+//     console.error('Reissue failed:', error);
+//     localStorage.removeItem('accessToken');
+//     window.location.href = '/login';
+//     return null;
+//   }
+// };
 
-const authMiddleware: Middleware = {
-  async onRequest({ schemaPath, request }: { schemaPath: string; request: Request }) {
-    if (UNPROTECTED_ROUTES.some((pathname) => schemaPath.startsWith(pathname))) {
-      return undefined;
-    }
+// const authMiddleware: Middleware = {
+//   async onRequest({ schemaPath, request }: { schemaPath: string; request: Request }) {
+//     if (UNPROTECTED_ROUTES.some((pathname) => schemaPath.startsWith(pathname))) {
+//       return undefined;
+//     }
 
-    let accessToken = getAccessToken();
+//     let accessToken = getAccessToken();
 
-    if (!accessToken) {
-      accessToken = await reissueToken();
+//     if (!accessToken) {
+//       accessToken = await reissueToken();
 
-      if (!accessToken) {
-        console.error('Access token reissue failed. Logging out...');
-        return request;
-      }
-    }
+//       if (!accessToken) {
+//         console.error('Access token reissue failed. Logging out...');
+//         return request;
+//       }
+//     }
 
-    request.headers.set('Authorization', `Bearer ${accessToken}`);
-    return request;
-  },
+//     request.headers.set('Authorization', `Bearer ${accessToken}`);
+//     return request;
+//   },
 
-  async onResponse({ request, response }) {
-    if (response.status === 401) {
-      console.warn('Access token expired. Attempting reissue...');
+//   async onResponse({ request, response }) {
+//     if (response.status === 401) {
+//       console.warn('Access token expired. Attempting reissue...');
 
-      const newAccessToken = await reissueToken();
+//       const newAccessToken = await reissueToken();
 
-      if (!newAccessToken) {
-        console.error('Reissue failed. Logging out...');
-        return response;
-      }
+//       if (!newAccessToken) {
+//         console.error('Reissue failed. Logging out...');
+//         return response;
+//       }
 
-      request.headers.set('Authorization', `Bearer ${newAccessToken}`);
-      return fetch(request);
-    }
-    return response;
-  },
-};
+//       request.headers.set('Authorization', `Bearer ${newAccessToken}`);
+//       return fetch(request);
+//     }
+//     return response;
+//   },
+// };
 
-export default authMiddleware;
+// export default authMiddleware;
--- apps/service/src/app/providers.tsx
+++ apps/service/src/app/providers.tsx
@@ -4,11 +4,11 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type * as React from 'react';
 
 import { client } from '@apis';
-import authMiddleware from '@/apis/authMiddleware';
+// import authMiddleware from '@/apis/authMiddleware';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
-  client.use(authMiddleware);
+  // client.use(authMiddleware);
 
   return (
     <QueryClientProvider client={queryClient}>
