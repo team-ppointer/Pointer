647f983c98357a7e4b7ef29ff084fe24f2b6922d
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-07-16 16:08:46 +0900

    feat(service): 마이페이지 수정, 로그아웃

--- apps/service/src/components/my-page/NameCard.tsx
+++ apps/service/src/components/my-page/NameCard.tsx
@@ -1,11 +1,17 @@
+'use client';
+
+import { getName, getGrade } from '@utils';
+
 const NameCard = () => {
+  const name = getName();
+  const grade = getGrade();
   return (
     <article className='center my-[2rem] flex gap-[0.8rem] rounded-[16px] bg-white p-[2rem]'>
       <p className='font-bold-18 text-black'>
-        <span className='text-main mr-[0.4rem]'>홍길동</span>
+        <span className='text-main mr-[0.4rem]'>{name}</span>
         <span>님</span>
       </p>
-      <p className='font-medium-14 text-lightgray500 flex items-end'>고등학교 2학년</p>
+      <p className='font-medium-14 text-lightgray500 flex items-end'>고등학교 {grade}학년</p>
     </article>
   );
 };
