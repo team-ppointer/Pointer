9afedf946b78f58e14ebfcffda31e30b82d29a3a
chaeyoung103 <thdcodud103@naver.com>
2025-07-18 10:13:40 +0900

    feat(service): sidebar 컴포넌트 구현

---
+++ apps/service/src/components/common/SideBar/SideBar.tsx
@@ -0,0 +1,61 @@
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
+
+import { IcCloseBig } from '@svg';
+
+import { Input } from '../Inputs';
+
+type SidebarProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export default function Sidebar({ isOpen, onClose }: SidebarProps) {
+  const [isVisible, setIsVisible] = useState(isOpen);
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsVisible(true); // 바로 보여주기
+    } else {
+      // 딜레이 후 visibility 끄기
+      const timeout = setTimeout(() => setIsVisible(false), 300); // 트랜지션 시간과 맞춰서
+      return () => clearTimeout(timeout);
+    }
+  }, [isOpen]);
+  return (
+    <>
+      {/* 오버레이: 모바일에서는 화면 전체, 데스크톱에서는 콘텐츠 영역에만 */}
+      <div
+        className={clsx(
+          'fixed inset-0 z-40 bg-black/30 transition-opacity duration-300',
+          {
+            'visible opacity-100': isOpen,
+            'invisible opacity-0': !isOpen,
+          },
+          'md:left-1/2 md:w-[768px] md:-translate-x-1/2 md:transform' // md 이상에서는 가운데 위치한 너비 제한된 오버레이
+        )}
+        onClick={onClose}
+      />
+
+      {/* 슬라이더 패널 */}
+      <div
+        className={clsx(
+          'fixed top-0 left-0 z-50 h-full w-[85%] transform bg-white transition-transform duration-300 md:absolute md:top-0 md:left-0 md:h-full',
+          {
+            'translate-x-0': isOpen,
+            '-translate-x-[100vw]': !isOpen,
+          }
+        )}>
+        <div className='p-[2rem]'>
+          <div className='flex items-center justify-between gap-[1.6rem]'>
+            <Input
+              className='bg-background h-[4.8rem] w-full rounded-[1.6rem] p-[1.6rem]'
+              placeholder='검색'
+            />
+            <IcCloseBig width={24} height={24} onClick={onClose} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
