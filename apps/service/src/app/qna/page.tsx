ae8ce20171bcc03b836449eea463dd86bf065ad1
chaeyoung103 <thdcodud103@naver.com>
2025-07-22 20:38:46 +0900

    feat(service): qna exist api 연결

--- apps/service/src/app/qna/page.tsx
+++ apps/service/src/app/qna/page.tsx
@@ -6,10 +6,21 @@ import Sidebar from '@/components/common/SideBar/SideBar';
 import { Header, Input } from '@components';
 import QnaList from '@/components/qna/QnaList';
 import { IcCloseBig } from '@svg';
+import useGetQnaExist from '@/apis/controller/qna/useGetQnaExist';
+import MyChat from '@/components/qna/chat/MyChat';
+import YourChat from '@/components/qna/chat/YourChat';
 
 const Page = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [search, setSearch] = useState<string>('');
+  const { data, isSuccess } = useGetQnaExist({
+    publishId: 1,
+    problemId: 3,
+    type: 'PROBLEM_CONTENT',
+  });
+
+  const qnaId = data?.id;
+
   return (
     <>
       <Header
@@ -18,7 +29,18 @@ const Page = () => {
         rightIconType='close'
         menuOnClick={() => setIsOpen(true)}
       />
-      <main className='relative flex h-dvh flex-col items-center justify-between px-[2rem] pt-[8rem] pb-[1.5rem]'>
+      <main className='relative flex h-dvh flex-col items-center justify-start px-[2rem] pt-[8rem] pb-[1.5rem]'>
+        {isSuccess && data.isExist ? (
+          <>
+            <MyChat>하이하이</MyChat>
+            <YourChat>안녕하세요</YourChat>
+          </>
+        ) : (
+          <p className='font-medium-16 text-lightgray500 flex h-full items-center justify-center text-center'>
+            등록된 질문이 없습니다.
+          </p>
+        )}
+
         <Sidebar isOpen={isOpen} onClose={() => setIsOpen(false)}>
           <div className='flex items-center justify-between gap-[1.6rem]'>
             <Input
@@ -28,7 +50,6 @@ const Page = () => {
               onKeyDown={(e) => {
                 if (e.key === 'Enter') {
                   setSearch(e.currentTarget.value);
-                  console.log(e.currentTarget.value);
                 }
               }}
             />
