4f7df648a962e2e853da8721ee84a323a62a756d
sterdsterd <sterdsterd@icloud.com>
2025-08-01 08:08:39 +0900

    feat: 개념 태그 API 연동

--- apps/admin/src/routes/_GNBLayout/concept-tags/index.tsx
+++ apps/admin/src/routes/_GNBLayout/concept-tags/index.tsx
@@ -4,6 +4,7 @@ import { Button, FloatingButton, Header, Input } from '@components';
 import { useForm } from 'react-hook-form';
 import { Divider } from '@repo/pointer-design-system/components';
 import { IcPencil } from '@svg';
+import { getConcept, getConceptCategory } from '@apis';
 
 import { ConceptTagCard } from '@/components/conceptTags';
 
@@ -11,12 +12,15 @@ export const Route = createFileRoute('/_GNBLayout/concept-tags/')({
   component: RouteComponent,
 });
 
-const OPTIONS = ['React', 'Vue', 'Svelte', 'Angular', 'aaaaa'];
-
 function RouteComponent() {
-  const [selectedTag, setSelectedTag] = useState<string[]>([]);
+  const [selectedTag, setSelectedTag] = useState<number[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
-  const toggleTag = (tag: string) => {
+  // api
+  const { data: concepts } = getConcept({ query: searchQuery });
+  const { data: conceptCategories } = getConceptCategory();
+
+  const toggleTag = (tag: number) => {
     setSelectedTag((prev) =>
       prev.includes(tag) ? prev.filter((item) => item !== tag) : [...prev, tag]
     );
@@ -27,7 +31,8 @@ function RouteComponent() {
   }>();
 
   const handleClickSearch = (data: { query: string }) => {
-    // Handle search logic here
+    setSearchQuery(data.query.trim());
+    setSelectedTag([]);
   };
 
   return (
@@ -43,54 +48,43 @@ function RouteComponent() {
       <div className='my-[4.8rem] flex items-center justify-between'>
         <h2 className='font-bold-32 text-black'>개념 태그 리스트</h2>
         <div className='flex gap-[1.6rem]'>
-          <Button sizeType='fit' variant='light'>
+          <Button sizeType='fit' variant='light' onClick={() => setSelectedTag([])}>
             전체 선택 해제
           </Button>
-          <Button sizeType='fit' variant='dark'>
+          <Button sizeType='fit' variant='dark' onClick={() => {}}>
             선택 태그 삭제
           </Button>
         </div>
       </div>
 
       <section className='mb-[8rem] flex flex-col gap-[4.8rem]'>
-        <article>
-          <div className='mb-[4rem] flex items-center gap-[1.2rem]'>
-            <h4 className='font-bold-24 text-black'>지수 로그 그래프</h4>
-            <IcPencil className='cursor-pointer' width={24} height={24} onClick={() => {}} />
-          </div>
-          <div className='grid grid-cols-3 gap-[4rem]'>
-            {OPTIONS.map((name) => {
-              const isChecked = selectedTag.includes(name);
-              return (
-                <ConceptTagCard
-                  key={name}
-                  name={name}
-                  isChecked={isChecked}
-                  toggleTag={toggleTag}
-                />
-              );
-            })}
-          </div>
-        </article>
-        <article>
-          <div className='mb-[4rem] flex items-center gap-[1.2rem]'>
-            <h4 className='font-bold-24 text-black'>지수 로그 그래프</h4>
-            <IcPencil className='cursor-pointer' width={24} height={24} onClick={() => {}} />
-          </div>
-          <div className='grid grid-cols-3 gap-[4rem]'>
-            {OPTIONS.map((name) => {
-              const isChecked = selectedTag.includes(name);
-              return (
-                <ConceptTagCard
-                  key={name}
-                  name={name}
-                  isChecked={isChecked}
-                  toggleTag={toggleTag}
-                />
-              );
-            })}
-          </div>
-        </article>
+        {conceptCategories?.data.map((category) =>
+          concepts?.data.some((concept) => concept.category.id === category.id) ? (
+            <article>
+              <div className='mb-[4rem] flex items-center gap-[1.2rem]'>
+                <h4 className='font-bold-24 text-black'>{category.name}</h4>
+                <IcPencil className='cursor-pointer' width={24} height={24} onClick={() => {}} />
+              </div>
+              <div className='grid grid-cols-3 gap-[4rem]'>
+                {concepts?.data
+                  .filter((concept) => concept.category.id === category.id)
+                  .map((concept) => {
+                    const isChecked = selectedTag.includes(concept.id);
+                    return (
+                      <ConceptTagCard
+                        key={concept.id}
+                        name={concept.name}
+                        isChecked={isChecked}
+                        toggleTag={() => toggleTag(concept.id)}
+                      />
+                    );
+                  })}
+              </div>
+            </article>
+          ) : (
+            <></>
+          )
+        )}
       </section>
 
       <FloatingButton onClick={() => {}}>새로운 개념 태그 등록하기</FloatingButton>
