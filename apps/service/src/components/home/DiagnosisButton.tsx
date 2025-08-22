73be7edf2126b243a3b4a1f48af443e1a62ee8f3
sterdsterd <sterdsterd@icloud.com>
2025-08-22 13:45:29 +0900

    design: DiagnosisButton date 위치 변경

--- apps/service/src/components/home/DiagnosisButton.tsx
+++ apps/service/src/components/home/DiagnosisButton.tsx
@@ -17,9 +17,17 @@ const DiagnosisButton = () => {
       href='/diagnosis'
       className='relative flex h-full flex-1 flex-col gap-[0.4rem] rounded-[1.6rem] bg-white px-[2.0rem] py-[1.8rem]'>
       <div className='flex w-full items-center justify-between gap-[0.8rem]'>
-        <div className='flex items-center gap-[0.8rem]'>
+        <div className='flex items-center gap-[0.6rem]'>
           {/* <IcNotice width={24} height={24} /> */}
           <span className='font-bold-16 text-main'>최근 진단</span>
+          {latestDiagnosis?.createdAt && (
+            <span className='font-medium-12 text-lightgray500'>
+              {new Date(latestDiagnosis.createdAt).toLocaleDateString('ko-KR', {
+                month: 'long',
+                day: 'numeric',
+              })}
+            </span>
+          )}
         </div>
         <IcNext width={24} height={24} className='text-main' />
       </div>
@@ -27,15 +35,6 @@ const DiagnosisButton = () => {
         <span className='font-medium-14 truncate text-black'>
           {latestDiagnosis ? latestDiagnosis.content : '최근 진단이 없어요'}
         </span>
-        {latestDiagnosis?.createdAt && (
-          <span className='font-medium-12 text-lightgray500'>
-            {new Date(latestDiagnosis.createdAt).toLocaleDateString('ko-KR', {
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric',
-            })}
-          </span>
-        )}
       </div>
     </Link>
   );
