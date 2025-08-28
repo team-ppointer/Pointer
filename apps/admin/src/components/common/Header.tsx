8c37a8a4dcc1b704353b6b80cb57fda8d5320dbe
sterdsterd <sterdsterd@icloud.com>
2025-08-28 14:50:20 +0900

    refactor: 문제 목록 페이지 디자인 업데이트

--- apps/admin/src/components/common/Header.tsx
+++ apps/admin/src/components/common/Header.tsx
@@ -1,24 +1,38 @@
-import { DeleteButton, PrevPageButton } from '@components';
+import { Button, DeleteButton, PrevPageButton } from '@components';
 
 interface HeaderProps {
   title: string;
   description?: string;
   deleteButton?: string;
   onClickDelete?: () => void;
+  actionButton?: string;
+  onClickAction?: () => void;
 }
 
-const Header = ({ title, description, deleteButton, onClickDelete }: HeaderProps) => {
+const Header = ({
+  title,
+  description,
+  deleteButton,
+  onClickDelete,
+  actionButton,
+  onClickAction,
+}: HeaderProps) => {
   return (
     <header>
-      <PrevPageButton />
-      <div className='mt-1200 flex w-full items-center justify-between'>
+      {/* <PrevPageButton /> */}
+      <div className='flex w-full items-center justify-between'>
         <div className='flex items-center gap-600'>
-          <h1 className='font-bold-32'>{title}</h1>
+          <h1 className='font-bold-18'>{title}</h1>
           {description && (
             <p className='font-medium-16 text-midgray100 whitespace-pre-line'>{description}</p>
           )}
         </div>
         {deleteButton && <DeleteButton label={deleteButton} onClick={onClickDelete} />}
+        {actionButton && (
+          <Button sizeType='fit' variant='dark' onClick={onClickAction}>
+            {actionButton}
+          </Button>
+        )}
       </div>
     </header>
   );
