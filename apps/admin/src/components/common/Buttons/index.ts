bb5e8fa4ce8f1918afef1eb56d0161fcd422b6fc
sterdsterd <sterdsterd@icloud.com>
2025-11-15 04:12:00 +0900

    refactor: remove PrevPageButton from exports to streamline button component usage

--- apps/admin/src/components/common/Buttons/index.ts
+++ apps/admin/src/components/common/Buttons/index.ts
@@ -3,6 +3,5 @@ import DeleteButton from './DeleteButton';
 import FloatingButton from './FloatingButton';
 import IconButton from './IconButton';
 import PlusButton from './PlusButton';
-import PrevPageButton from './PrevPageButton';
 
-export { Button, DeleteButton, FloatingButton, IconButton, PlusButton, PrevPageButton };
+export { Button, DeleteButton, FloatingButton, IconButton, PlusButton };
