d34d3b84b9ef73e6328a752ced98af46613a4f08
sterdsterd <sterdsterd@icloud.com>
2025-11-15 12:50:39 +0900

    refactor: remove PlusButton component and update button exports for cleaner code structure

--- apps/admin/src/components/common/Buttons/index.ts
+++ apps/admin/src/components/common/Buttons/index.ts
@@ -2,6 +2,5 @@ import Button from './Button';
 import DeleteButton from './DeleteButton';
 import FloatingButton from './FloatingButton';
 import IconButton from './IconButton';
-import PlusButton from './PlusButton';
 
-export { Button, DeleteButton, FloatingButton, IconButton, PlusButton };
+export { Button, DeleteButton, FloatingButton, IconButton };
