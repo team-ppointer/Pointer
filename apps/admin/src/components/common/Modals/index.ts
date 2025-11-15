178d468e5cb24458d159f50f0e54fb767425d2f3
sterdsterd <sterdsterd@icloud.com>
2025-11-15 13:07:42 +0900

    refactor: remove CreateNoticeModal and NoticeListModal components for code cleanup and improved modularity

--- apps/admin/src/components/common/Modals/index.ts
+++ apps/admin/src/components/common/Modals/index.ts
@@ -3,12 +3,10 @@ import BaseModalTemplate from './templates/BaseModalTemplate';
 import ErrorModalTemplate from './templates/ErrorModalTemplate';
 import OneButtonModalTemplate from './templates/OneButtonModalTemplate';
 import TwoButtonModalTemplate from './templates/TwoButtonModalTemplate';
-import CreateNoticeModal from './CreateNoticeModal';
 import CreatePracticeTestModal from './CreatePracticeTestModal';
 import EditCategoryModal from './EditCategoryModal';
 import EditConceptModal from './EditConceptModal';
 import EditTeacherModal from './EditTeacherModal';
-import NoticeListModal from './NoticeListModal';
 import ProblemSearchModal from './ProblemSearchModal';
 import ProgressModal from './ProgressModal';
 import SelectStudentModal from './SelectStudentModal';
@@ -21,12 +19,10 @@ export {
   ErrorModalTemplate,
   OneButtonModalTemplate,
   TwoButtonModalTemplate,
-  CreateNoticeModal,
   CreatePracticeTestModal,
   EditCategoryModal,
   EditConceptModal,
   EditTeacherModal,
-  NoticeListModal,
   ProblemSearchModal,
   ProgressModal,
   SelectStudentModal,
