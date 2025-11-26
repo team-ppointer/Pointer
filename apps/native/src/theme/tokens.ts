0423715fb7034e3de94bfefdea367034a0b38e0c
sterdsterd <sterdsterd@icloud.com>
2025-11-27 02:34:57 +0900

    feat(native): update tailwind configuration to use theme tokens for colors, font family, and font size

---
+++ apps/native/src/theme/tokens.ts
@@ -0,0 +1,117 @@
+export const colors = {
+  // Grayscale
+  'light-gray-500': '#C6CAD4',
+  'light-gray-400': '#DFE2E7',
+  'light-gray-300': '#EDEEF2',
+  'light-gray-200': '#F3F5FB',
+  'light-gray-100': '#F8F9FC',
+
+  'mid-gray-200': '#6B6F77',
+  'mid-gray-100': '#9FA4AE',
+
+  'dark-gray-200': '#222224',
+  'dark-gray-100': '#3E3F45',
+
+  background: '#F8F9FC',
+
+  // Colors
+  green: '#0FB700',
+  'light-green': '#E1F5DF',
+
+  red: '#D20000',
+  'light-red': '#FCE4E4',
+  'red-delete': '#FF3B30',
+
+  'blue-100': '#3A67EE',
+  'blue-200': '#2653DA',
+  'blue-300': '#123FC6',
+  'light-blue-100': '#ECF0FB',
+  'light-blue-200': '#D6E1FF',
+
+  // Brand Colors
+  main: '#617AF9',
+  "main-2": "#526BEA",
+  "sub-1": "#C5CEFF",
+  "sub-2": "#E9EBF8",
+  yellow: '#E59C00',
+  'light-yellow': '#FFF4CC',
+};
+
+export const newColors = {
+  // Grayscale
+  "gray-100": "#F8F9FC", // 기존 lightgray100
+  "gray-200": "#F3F5FB", // 기존 lightgray200
+  "gray-300": "#EDEEF2", // 기존 lightgray300
+  "gray-400": "#DFE2E7", // 기존 lightgray400
+  "gray-500": "#C6CAD4", // 기존 lightgray500
+  "gray-600": "#9FA4AE", // 기존 midgray100
+  "gray-700": "#6B6F77", // 기존 midgray200
+  "gray-800": "#3E3F45", // 기존 darkgray100
+  "gray-900": "#222224", // 기존 darkgray200
+
+  // Colors
+  "green-100": "#E7F6E5", // 기존 light green
+  "green-500": "#0FB700", // 기존 green
+  "red-100": "#FCE4E4", // 기존 light red
+  "red-400": "#FF3B30", // 기존 red-delete
+  "red-500": "#D20000", // 기존 red
+  "blue-100": "#ECF0FB", // 기존 light blue 100
+  "blue-200": "#D6E1FF", // 기존 light blue 200
+  "blue-500": "#3A67EE", // 기존 blue100
+  "blue-600": "#2653DA", // 기존 blue200
+  "blue-700": "#123FC6", // 기존 blue300
+
+  // Brand Colors
+  "primary-100": "#E9EBF8", // 기존 sub color1
+  "primary-200": "#C5CEFF", // 기존 sub color2
+  "primary-500": "#617AF9", // 기존 main color
+  "primary-600": "#526BEA", // 기존 main color2
+  "secondary-100": "#FFF4CC", // 기존 light yellow
+  "secondary-500": "#E59C00", // 기존 yellow
+};
+
+export const fontFamily = {
+  pretendard: ['Pretendard'],
+  pretendardMedium: ['PretendardMedium'],
+  pretendardSemiBold: ['PretendardSemiBold'],
+  pretendardBold: ['PretendardBold'],
+};
+
+export const fontSize = {
+  // Bold 700, Semibold 600, Medium 500, Regular 400
+  '32b': ['32px', { fontWeight: '700', lineHeight: '150%' }],
+
+  '24b': ['24px', { fontWeight: '700', lineHeight: '150%' }],
+
+  '20b': ['20px', { fontWeight: '700', lineHeight: '150%' }],
+  '20r': ['20px', { fontWeight: '400', lineHeight: '150%' }],
+
+  '18b': ['18px', { fontWeight: '700', lineHeight: '150%' }],
+  '18m': ['18px', { fontWeight: '500', lineHeight: '150%' }],
+
+  '16b': ['16px', { fontWeight: '700', lineHeight: '150%' }],
+  '16sb': ['16px', { fontWeight: '600', lineHeight: '150%' }],
+  '16m': ['16px', { fontWeight: '500', lineHeight: '150%' }],
+  '16r': ['16px', { fontWeight: '400', lineHeight: '150%' }],
+
+  '14b': ['14px', { fontWeight: '700', lineHeight: '150%' }],
+  '14sb': ['14px', { fontWeight: '600', lineHeight: '150%' }],
+  '14m': ['14px', { fontWeight: '500', lineHeight: '150%' }],
+  '14r': ['14px', { fontWeight: '400', lineHeight: '150%' }],
+
+  '13b': ['13px', { fontWeight: '700', lineHeight: '150%' }],
+  '13m': ['13px', { fontWeight: '500', lineHeight: '150%' }],
+  '13r': ['13px', { fontWeight: '400', lineHeight: '150%' }],
+
+  '12m': ['12px', { fontWeight: '500', lineHeight: '150%' }],
+  '12r': ['12px', { fontWeight: '400', lineHeight: '150%' }],
+
+  '10m': ['10px', { fontWeight: '500', lineHeight: '150%' }],
+  '10r': ['10px', { fontWeight: '400', lineHeight: '150%' }],
+};
+
+export const screens = {
+  "sm": "0px",
+  "md": "768px",
+  "lg": "1024px",
+}
--- apps/native/tailwind.config.js
+++ apps/native/tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import { newColors, fontFamily, fontSize, screens } from './src/theme/tokens';
+
 module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{js,jsx,ts,tsx}', './App.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
-      fontFamily: {
-        pretendard: ['Pretendard'],
-        pretendardMedium: ['PretendardMedium'],
-        pretendardSemiBold: ['PretendardSemiBold'],
-        pretendardBold: ['PretendardBold'],
-      },
+      colors: newColors,
+      fontFamily,
+      fontSize,
+      screens,
     },
   },
   plugins: [],
