9f4943437a6916c863a95006918209ee193f4020
b0nsu <125778250+b0nsu@users.noreply.github.com>
2025-11-27 13:16:16 +0900

    feat: add stack navigator between home screen and notifications page

--- apps/native/src/navigation/RootNavigator.tsx
+++ apps/native/src/navigation/RootNavigator.tsx
@@ -3,9 +3,11 @@ import { Platform } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createStackNavigator } from '@react-navigation/stack';
 import StudentNavigator from './student/StudentNavigator';
+import NotificationsScreen from '@/features/student/home/screens/NotificationsScreen';
 
 export type RootStackParamList = {
   StudentNavigator: undefined;
+  Notifications: undefined;
 };
 
 const NativeStack = createNativeStackNavigator<RootStackParamList>();
@@ -16,6 +18,7 @@ const RootNavigator = () => {
     return (
       <WebStack.Navigator screenOptions={{ headerShown: false }}>
         <WebStack.Screen name='StudentNavigator' component={StudentNavigator} />
+        <WebStack.Screen name='Notifications' component={NotificationsScreen} />
       </WebStack.Navigator>
     );
   }
@@ -23,6 +26,7 @@ const RootNavigator = () => {
   return (
     <NativeStack.Navigator screenOptions={{ headerShown: false }}>
       <NativeStack.Screen name='StudentNavigator' component={StudentNavigator} />
+      <NativeStack.Screen name='Notifications' component={NotificationsScreen} />
     </NativeStack.Navigator>
   );
 };
--- apps/native/src/navigation/student/StudentNavigator.tsx
+++ apps/native/src/navigation/student/StudentNavigator.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Text, View, Image } from 'react-native';
-import { Bell, Bookmark, Home, Menu, MessageCircleMore } from 'lucide-react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { Bell, Bookmark, ChevronLeft, Home, Menu, MessageCircleMore } from 'lucide-react-native';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { newColors } from '@/theme/tokens';
 import HomeScreen from '../../features/student/home/screens/HomeScreen';
 import ScrapScreen from '../../features/student/scrap/screens/ScrapScreen';
 import QnaScreen from '../../features/student/qna/screens/QnaScreen';
 import MenuScreen from '../../features/student/menu/screens/MenuScreen';
+import {
+  createNativeStackNavigator,
+  NativeStackHeaderProps,
+  NativeStackNavigationProp,
+} from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+import { RootStackParamList } from '../RootNavigator';
+import NotificationScreen from '@/features/student/home/screens/NotificationsScreen';
 
 export type StudentTabParamList = {
   Home: undefined;
@@ -16,18 +24,69 @@ export type StudentTabParamList = {
   AllMenu: undefined;
 };
 
-const Tab = createBottomTabNavigator<StudentTabParamList>();
+const BrandHeader = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
-const BrandHeader = () => (
-  <SafeAreaView edges={['top']} className='bg-blue-100'>
-    <View className='flex-row justify-between px-6 py-3.5 md:px-[60px] lg:px-32'>
-      <Image className='h-[40px]' source={require('../../../assets/images/pointer-logo.png')} />
-      <View className='h-[48px] w-[48px] gap-[10px] rounded-[8px] px-[3px] py-[9px]'>
-        <Bell className='h-[24px]' style={{ aspectRatio: 1 }} color='#0C0C0D' />
+  const goToNotifications = () => {
+    navigation.push('Notifications');
+  };
+  return (
+    <SafeAreaView edges={['top']} className='bg-blue-100'>
+      <View className='flex-row justify-between px-6 py-3.5 md:px-[60px] lg:px-32'>
+        <Image className='h-[40px]' source={require('../../../assets/images/pointer-logo.png')} />
+        <TouchableOpacity
+          onPress={goToNotifications}
+          className='h-[48px] w-[48px] gap-[10px] rounded-[8px] px-[3px] py-[9px]'>
+          <Bell className='h-[24px]' style={{ aspectRatio: 1 }} color='#0C0C0D' />
+        </TouchableOpacity>
       </View>
-    </View>
-  </SafeAreaView>
-);
+    </SafeAreaView>
+  );
+};
+
+const NotificationHeader = ({ navigation, back }: NativeStackHeaderProps) => {
+  return (
+    <SafeAreaView edges={['top']} className=''>
+      <View className='flex-row items-center justify-between px-5 py-3.5'>
+        {back ? (
+          <TouchableOpacity onPress={navigation.goBack} className='p-2'>
+            <ChevronLeft className='text-black' size={32} />
+          </TouchableOpacity>
+        ) : (
+          <View className='h-[48px] w-[48px] gap-[10px]' /> // 빈 공간
+        )}
+        <Text className='text-20b text-gray-900' style={{ lineHeight: 30 }}>
+          알림
+        </Text>
+        <View className='h-[48px] w-[48px] gap-[10px]' /> // 오른쪽 빈 공간
+      </View>
+    </SafeAreaView>
+  );
+};
+/* StackNavigator for Home <-> Notifications */
+
+const Stack = createNativeStackNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='HomeMain'
+        component={HomeScreen}
+        options={{ header: () => <BrandHeader /> }} // 홈만 BrandHeader
+      />
+      <Stack.Screen
+        name='Notifications'
+        component={NotificationScreen}
+        options={{ header: (props) => <NotificationHeader {...props} /> }} // 알림 화면은 기본 헤더
+      />
+    </Stack.Navigator>
+  );
+};
+
+/* StudentNavigator for Tabs */
+
+const Tab = createBottomTabNavigator<StudentTabParamList>();
 
 const tabLabel =
   (label: string) =>
@@ -63,10 +122,9 @@ const StudentNavigator = () => {
       }}>
       <Tab.Screen
         name='Home'
-        component={HomeScreen}
+        component={HomeStack}
         options={{
-          header: () => <BrandHeader />,
-          headerShown: true,
+          headerShown: false,
           tabBarLabel: tabLabel('홈'),
           tabBarIcon: ({ focused }) => (
             <Home color={focused ? newColors['primary-500'] : newColors['gray-800']} size={22} />
