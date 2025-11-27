927a356d0827f6f414c5ab7f12105281a3bb458a
b0nsu <125778250+b0nsu@users.noreply.github.com>
2025-11-27 13:16:07 +0900

    feat: implement notifications page

---
+++ apps/native/src/features/student/home/screens/NotificationsScreen.tsx
@@ -0,0 +1,69 @@
+import Container from '@/components/common/Container';
+import NotificationItem from '@/components/common/NotificationItem';
+import { ChevronRight } from 'lucide-react-native';
+import { View, Text, ScrollView, Pressable } from 'react-native';
+
+const NotificationScreen = () => {
+  return (
+    <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+      <View className='mx-auto w-full'>
+        <Container className='gap-[10px] pt-[16px]'>
+          <Text className='text-20b text-gray-900'>공지</Text>
+          <NotificationItem
+            icon='megaphone'
+            title='공지 제목이 작성돼요.'
+            time='12월 4일'
+            hasShadow={true}>
+            <NotificationItem.Button variant='ghost'>더보기</NotificationItem.Button>
+          </NotificationItem>
+          <NotificationItem
+            icon='megaphone'
+            title='공지 제목이 작성돼요.'
+            time='12월 4일'
+            hasShadow={true}>
+            <NotificationItem.Button variant='ghost'>더보기</NotificationItem.Button>
+          </NotificationItem>
+        </Container>
+        <Container className='gap-[10px] pt-[26px]'>
+          <View className='flex-row items-center justify-between'>
+            <Text className='text-20b text-gray-900'>알림</Text>
+            <Pressable className='px-2'>
+              <Text className='text-12sb text-blue-500'>모두 읽음</Text>
+            </Pressable>
+          </View>
+
+          <NotificationItem
+            icon='message'
+            title='12월 첫째 주 출제진의 피드백이 도착했어요.'
+            time='오늘 16:48'
+            hasBadge={true}
+            hasShadow={true}>
+            <NotificationItem.Button icon={ChevronRight} variant='outline'>
+              더보기
+            </NotificationItem.Button>
+          </NotificationItem>
+          <NotificationItem
+            icon='book'
+            title='오늘의 문제 세트가 도착했어요.'
+            time='오늘 12:00'
+            hasBadge={true}
+            hasShadow={true}>
+            <NotificationItem.Button>문제풀기</NotificationItem.Button>
+          </NotificationItem>
+          <NotificationItem
+            icon='book'
+            title='11월 28일의 문제 세트가 ###님을 기다리고 있어요.'
+            time='12월 2일'
+            hasShadow={true}>
+            <NotificationItem.Button>문제풀기</NotificationItem.Button>
+          </NotificationItem>
+        </Container>
+      </View>
+      <Container className='flex-1 items-center justify-center gap-[10px] pb-[100px] pt-[20px]'>
+        <Text className='text-14m text-gray-600'>7일 전 알림까지 확인할 수 있어요.</Text>
+      </Container>
+    </ScrollView>
+  );
+};
+
+export default NotificationScreen;
