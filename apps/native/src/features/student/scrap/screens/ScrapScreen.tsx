f0973733504b63f349b8d41df5767c35844e64ce
b0nsu <125778250+b0nsu@users.noreply.github.com>
2025-12-13 01:36:17 +0900

    feat: add sorting functionality, refactor: change ScrapGrid data prop

--- apps/native/src/features/student/scrap/screens/ScrapScreen.tsx
+++ apps/native/src/features/student/scrap/screens/ScrapScreen.tsx
@@ -2,49 +2,44 @@ import { Container, LoadingScreen } from '@/components/common';
 import { StudentRootStackParamList } from '@/navigation/student/types';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import React, { useReducer, useState } from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useReducer, useState } from 'react';
+import { View, Text, Pressable } from 'react-native';
 import { reducer, selectState } from '../utils/reducer';
-import ScrapHeader from '../components/ScrapHeader';
-import ScrapGrid, { DummyItem } from '../components/ScrapItemGrid';
-import SortDropdown from '../components/SortDropdown';
-
-const folderDummy: DummyItem[] = Array.from({ length: 13 }, (_, i) => {
-  if (i % 3 === 0) {
-    return {
-      type: 'Folder',
-      id: i.toString(),
-      title: '폴더명',
-      amount: 127 + i,
-    };
-  } else {
-    return {
-      type: 'Scrap',
-      id: i.toString(),
-      title: '스크랩명',
-      timestamp: 202511191130 - i * 11,
-    };
-  }
-});
+import ScrapHeader from '../components/Header/ScrapHeader';
+import { ScrapGrid } from '../components/ScrapItemGrid';
+import SortDropdown from '../components/Modal/SortDropdown';
+import { SortKey, SortOrder, sortScrapData } from '../utils/sortScrap';
+import { ChevronDownFilledIcon, ChevronUpFilledIcon } from '@/components/system/icons';
+import { useScrapStore } from '@/stores';
+import { showToast } from '../components/Modal/Toast';
+import { folderDummy } from '../utils/testdataset';
 
 const ScrapScreen = () => {
   const [reducerState, dispatch] = useReducer(reducer, selectState);
-  const [data, setData] = useState(folderDummy);
   const [fetchloading, setFetchLoading] = useState(false);
-
+  const [sortKey, setSortKey] = useState<SortKey>('TYPE'); // 기본: 유형순
+  const [sortOrder, setSortOrder] = useState<SortOrder>('ASC'); // 기본: 오름차순
   const navigation = useNavigation<NativeStackNavigationProp<StudentRootStackParamList>>();
 
-  const orderList = [
-    { label: '유형순', value: 'type' },
-    { label: '이름순', value: 'name' },
-    { label: '최신순', value: 'latest' },
-  ];
-  const [order, setOrder] = useState({
-    label: '유형순',
-    value: 'type',
-  });
+  const data = useScrapStore((state) => state.data);
+  const setData = useScrapStore((state) => state.setData);
+  const deleteItem = useScrapStore((state) => state.deleteItem);
 
-  const isallSelected = reducerState.selectedItems.length === folderDummy.length;
+  // Fetching Data
+  useEffect(() => {
+    setFetchLoading(true);
+
+    setTimeout(() => {
+      setData(folderDummy); // 초기값 세팅
+      setFetchLoading(false);
+    }, 800);
+  }, []);
+
+  useEffect(() => {
+    setData(sortScrapData(data, sortKey, sortOrder));
+  }, [sortKey, sortOrder]);
+
+  const isallSelected = reducerState.selectedItems.length === data.length;
 
   return (
     <View className='w-full flex-1 bg-gray-100'>
@@ -61,34 +56,40 @@ const ScrapScreen = () => {
         onMove={() => {
           const selectedSet = new Set(reducerState.selectedItems);
           const selectedFolders = folderDummy.filter(
-            (item) => selectedSet.has(item.id) && item.type === 'Folder'
+            (item) => selectedSet.has(item.id) && item.type === 'FOLDER'
           );
-          if (selectedFolders.length > 0) console.log(selectedFolders);
+          if (selectedFolders.length > 0) showToast('error', '스크랩만 이동이 가능합니다.');
         }}
         onDelete={async () => {
           setFetchLoading(true);
-
           try {
-            const selectedSet = new Set(reducerState.selectedItems);
-
-            await new Promise((resolve) => setTimeout(resolve, 500));
-
-            setData((prev) => prev.filter((item) => !selectedSet.has(item.id)));
+            await new Promise((resolve) => setTimeout(resolve, 100));
+            deleteItem(reducerState.selectedItems);
           } finally {
             setFetchLoading(false);
             dispatch({ type: 'CLEAR_SELECTION' });
+            showToast('success', '휴지통으로 이동해 한 달 후 영구 삭제됩니다.');
           }
         }}
       />
       <View className='bg-gray-100'>
         <Container className='items-end gap-[10px] py-[10px]'>
-          <SortDropdown orderList={orderList} order={order} setOrder={setOrder} />
+          <View className='flex-row items-center justify-between'>
+            <SortDropdown orderValue={sortKey} setOrderValue={setSortKey} />
+            <Pressable onPress={() => setSortOrder((prev) => (prev === 'ASC' ? 'DESC' : 'ASC'))}>
+              {sortOrder === 'ASC' ? <ChevronUpFilledIcon /> : <ChevronDownFilledIcon />}
+            </Pressable>
+          </View>
         </Container>
         <Container className='pb-[120px] pt-4'>
           {fetchloading ? (
             <LoadingScreen label='데이터를 불러오고 있습니다.' />
           ) : (
-            <ScrapGrid data={data} state={reducerState} dispatch={dispatch} />
+            <ScrapGrid
+              data={[{ ADD: true } as any, ...data]}
+              state={reducerState}
+              dispatch={dispatch}
+            />
           )}
         </Container>
       </View>
