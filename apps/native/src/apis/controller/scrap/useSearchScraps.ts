adbab4a382729daf7401eb7845a5ca9e13c53f61
b0nsu <125778250+b0nsu@users.noreply.github.com>
2025-12-19 23:18:50 +0900

    refactor(api): modify fetch query parameters

--- apps/native/src/apis/controller/scrap/useSearchScraps.ts
+++ apps/native/src/apis/controller/scrap/useSearchScraps.ts
@@ -1,22 +1,19 @@
-import { useQuery } from '@tanstack/react-query';
-import { client } from '@/apis/client';
+import { TanstackQueryClient } from '@apis';
 import { paths } from '@/types/api/schema';
 
-type SearchScrapsParams = paths['/api/student/scrap/search/all']['get']['parameters']['query'];
-type SearchScrapsResponse =
-  paths['/api/student/scrap/search/all']['get']['responses']['200']['content']['*/*'];
+type SearchScrapsParams = paths['/api/student/scrap/search']['get']['parameters']['query'];
 
 export const useSearchScraps = (params: SearchScrapsParams = {}, enabled = true) => {
-  return useQuery({
-    queryKey: ['scrap', 'search', params],
-    queryFn: async (): Promise<SearchScrapsResponse> => {
-      const { data } = await client.GET('/api/student/scrap/search/all', {
-        params: {
-          query: params,
-        },
-      });
-      return data as SearchScrapsResponse;
+  return TanstackQueryClient.useQuery(
+    'get',
+    '/api/student/scrap/search',
+    {
+      params: {
+        query: params,
+      },
     },
-    enabled,
-  });
+    {
+      enabled,
+    }
+  );
 };
