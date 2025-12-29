974df7062ac0c2b30b1549ea7dcebcd399591054
b0nsu <125778250+b0nsu@users.noreply.github.com>
2025-12-30 00:36:13 +0900

    feat: implement recent scrap

---
+++ apps/native/src/features/student/scrap/components/Card/cards/RecentScrapCard.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Pressable, View, Text, ImageBackground } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+import { StudentRootStackParamList } from '@/navigation/student/types';
+import type { ScrapDetailResp } from '@/features/student/scrap/utils/types';
+import { useNoteStore } from '@/stores/scrapNoteStore';
+import { useRecentScrapStore } from '@/stores/recentScrapStore';
+
+type RecentScrapCardProps = {
+  scrap: ScrapDetailResp & { type: 'SCRAP' };
+};
+
+export const RecentScrapCard = ({ scrap }: RecentScrapCardProps) => {
+  const navigation = useNavigation<NativeStackNavigationProp<StudentRootStackParamList>>();
+  const openNote = useNoteStore((state) => state.openNote);
+  const addScrap = useRecentScrapStore((state) => state.addScrap);
+
+  return (
+    <Pressable
+      onPress={() => {
+        openNote({ id: scrap.id, title: scrap.name ?? '' });
+        addScrap(scrap.id);
+        navigation.push('ScrapContentDetail', { id: scrap.id });
+      }}
+      className='bg-primary-200 h-[140px] w-[140px] flex-col items-center justify-end rounded-[12px] border border-gray-300'>
+      <ImageBackground
+        source={{ uri: scrap.thumbnailUrl }}
+        className='rounded-t-[12px]'
+        style={{ width: '100%', height: '100%' }}
+        resizeMode='cover'
+      />
+      <View className='h-[100px] w-full justify-between rounded-b-[12px] bg-white p-[10px]'>
+        <Text className='text-16sb text-black' numberOfLines={2}>
+          {scrap.name}
+        </Text>
+        <Text className='text-12r text-gray-700 ' numberOfLines={1}>
+          {scrap.updatedAt}
+        </Text>
+      </View>
+    </Pressable>
+  );
+};
