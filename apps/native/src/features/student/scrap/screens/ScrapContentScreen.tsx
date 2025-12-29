481ef949ae3f96fbe0721c185753dec934fd183b
b0nsu <125778250+b0nsu@users.noreply.github.com>
2025-12-30 00:41:33 +0900

    fix: update MoveScrapModal props

--- apps/native/src/features/student/scrap/screens/ScrapContentScreen.tsx
+++ apps/native/src/features/student/scrap/screens/ScrapContentScreen.tsx
@@ -45,71 +45,74 @@ const ScrapContentScreen = () => {
     reducerState.selectedItems.length === contents.length && contents.length > 0;
 
   return (
-    <View className='w-full flex-1 bg-gray-100'>
-      <ScrapHeader
-        reducerState={reducerState}
-        title={folder?.name}
-        navigateback={navigation}
-        navigateSearchPress={() => navigation.push('SearchScrap')}
-        navigateTrashPress={() => navigation.push('DeletedScrap')}
-        onEnterSelection={() => dispatch({ type: 'ENTER_SELECTION' })}
-        onExitSelection={() => dispatch({ type: 'EXIT_SELECTION' })}
-        isAllSelected={isAllSelected}
-        onSelectAll={() => {
-          const allItems = contents.map((item) => ({ id: item.id, type: item.type }));
-          dispatch({ type: 'SELECT_ALL', allItems: isAllSelected ? [] : allItems });
-        }}
-        onMove={() => {
-          const selectedFolders = reducerState.selectedItems.filter(
-            (selected) => selected.type === 'FOLDER'
-          );
-          if (selectedFolders.length > 0) {
-            showToast('error', '스크랩만 이동이 가능합니다.');
-            return;
-          }
-          if (reducerState.selectedItems.length === 0) {
-            showToast('error', '이동할 스크랩을 선택해주세요.');
-            return;
-          }
-          setIsMoveModalVisible(true);
-        }}
-        onDelete={async () => {
-          if (reducerState.selectedItems.length === 0) {
-            showToast('error', '삭제할 항목을 선택해주세요.');
-            return;
-          }
+    <>
+      <View className='w-full flex-1 bg-gray-100'>
+        <ScrapHeader
+          reducerState={reducerState}
+          title={folder?.name}
+          navigateback={navigation}
+          navigateSearchPress={() => navigation.push('SearchScrap')}
+          navigateTrashPress={() => navigation.push('DeletedScrap')}
+          onEnterSelection={() => dispatch({ type: 'ENTER_SELECTION' })}
+          onExitSelection={() => dispatch({ type: 'EXIT_SELECTION' })}
+          isAllSelected={isAllSelected}
+          onSelectAll={() => {
+            const allItems = contents.map((item) => ({ id: item.id, type: item.type }));
+            dispatch({ type: 'SELECT_ALL', allItems: isAllSelected ? [] : allItems });
+          }}
+          onMove={() => {
+            const selectedFolders = reducerState.selectedItems.filter(
+              (selected) => selected.type === 'FOLDER'
+            );
+            if (selectedFolders.length > 0) {
+              showToast('error', '스크랩만 이동이 가능합니다.');
+              return;
+            }
+            if (reducerState.selectedItems.length === 0) {
+              showToast('error', '이동할 스크랩을 선택해주세요.');
+              return;
+            }
+            setIsMoveModalVisible(true);
+          }}
+          onDelete={async () => {
+            if (reducerState.selectedItems.length === 0) {
+              showToast('error', '삭제할 항목을 선택해주세요.');
+              return;
+            }
 
-          try {
-            const items = reducerState.selectedItems;
+            try {
+              const items = reducerState.selectedItems;
 
-            await deleteScrap({ items });
+              await deleteScrap({ items });
 
-            dispatch({ type: 'CLEAR_SELECTION' });
-            showToast('success', '휴지통으로 이동해 한 달 후 영구 삭제됩니다.');
-          } catch (error: any) {
-            showToast('error', '삭제 중 오류가 발생했습니다.');
-          }
-        }}
-      />
-      <View className='bg-gray-100'>
-        <Container className='items-end gap-[10px] py-[10px]'>
-          <SortDropdown
-            ordertype={'CONTENT'}
-            orderValue={sortKey}
-            setOrderValue={setSortKey}
-            sortOrder={sortOrder}
-            setSortOrder={setSortOrder}
-          />
-        </Container>
-        <Container className='pb-[120px] pt-4'>
-          {isLoading ? (
-            <LoadingScreen label='데이터를 불러오고 있습니다.' />
-          ) : (
-            <ScrapGrid data={sortedData} reducerState={reducerState} dispatch={dispatch} />
-          )}
-        </Container>
+              dispatch({ type: 'CLEAR_SELECTION' });
+              showToast('success', '휴지통으로 이동해 한 달 후 영구 삭제됩니다.');
+            } catch (error: any) {
+              showToast('error', '삭제 중 오류가 발생했습니다.');
+            }
+          }}
+        />
+        <View className='bg-gray-100'>
+          <Container className='items-end gap-[10px] py-[10px]'>
+            <SortDropdown
+              ordertype={'CONTENT'}
+              orderValue={sortKey}
+              setOrderValue={setSortKey}
+              sortOrder={sortOrder}
+              setSortOrder={setSortOrder}
+            />
+          </Container>
+          <Container className='pb-[120px] pt-4'>
+            {isLoading ? (
+              <LoadingScreen label='데이터를 불러오고 있습니다.' />
+            ) : (
+              <ScrapGrid data={sortedData} reducerState={reducerState} dispatch={dispatch} />
+            )}
+          </Container>
+        </View>
       </View>
       <MoveScrapModal
+        currentFolderId={Number(id)}
         visible={isMoveModalVisible}
         onClose={() => setIsMoveModalVisible(false)}
         selectedItems={reducerState.selectedItems}
@@ -118,7 +121,7 @@ const ScrapContentScreen = () => {
           refetch();
         }}
       />
-    </View>
+    </>
   );
 };
 
