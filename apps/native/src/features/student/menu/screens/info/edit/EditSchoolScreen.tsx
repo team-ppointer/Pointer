7684b683788b7301ff701374cfb5cd139ea3435d
b0nsu <125778250+b0nsu@users.noreply.github.com>
2026-01-20 17:58:16 +0900

    refactor(native): update EditSchoolScreen to handle optional schoolId and remove unnecessary error handling for improved code clarity

--- apps/native/src/features/student/menu/screens/info/edit/EditSchoolScreen.tsx
+++ apps/native/src/features/student/menu/screens/info/edit/EditSchoolScreen.tsx
@@ -47,13 +47,13 @@ const EditSchoolScreen = ({
   };
 
   const handleSave = async () => {
-    if (!schoolId) {
-      showToast('error', '학교를 선택해 주세요.');
-      return;
-    }
+    // if (!schoolId) {
+    //   showToast('error', '학교를 선택해 주세요.');
+    //   return;
+    // }
 
     putMeMutate(
-      { schoolId },
+      { schoolId: schoolId || undefined },
       {
         onSuccess: () => {
           navigation.push('EditGrade', { initialGrade: route.params.initialSchool?.grade });
@@ -75,7 +75,6 @@ const EditSchoolScreen = ({
       title='현재 재학중인 학교명을 입력해 주세요.'
       description='학교를 입력해 맞춤형 문제를 제공받아요.'
       onPressCTA={handleSave}
-      ctaDisabled={!schoolId}
       skipLabel='건너뛰기'
       onSkip={handleSkip}>
       <View>
